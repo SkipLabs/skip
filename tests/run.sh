e27a57e7652a5cd3607982a0bd7b8272ffc510b0
Julien Verlaguet <julien.verlaguet@gmail.com>
2022-08-03 17:58:42 -0400

    progress on tests

--- tests/run.sh
+++ tests/run.sh
@@ -3,42 +3,38 @@
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
-
-col=80
-
-
+COL=80
 CC=clang-10
 CPP=clang++-10
 OLEVEL=-O0
 RUNTIME=../build/libskip_runtime64.a
 
-TEST_EXP="${1%.out}.exp"
+TEST_EXP="${1%.test}.exp"
 EXP=${TEST_EXP#"../build/tests/"}
-TEST_EXP_ERR="${1%.out}.exp_err"
+TEST_EXP_ERR="${1%.test}.exp_err"
 EXP_ERR=${TEST_EXP_ERR#"../build/tests/"}
-ERR="${1%.out}.err"
+ERR="${1%.test}.err"
 
 if test -f "$EXP_ERR"; then
     diff -B -w $EXP_ERR $ERR > /dev/null
     if [ $? -eq 0 ]; then
         touch $1
-        printf '%s%s%*s%s\n' "$1" "$GREEN" $((col-${#1})) "[OK]" "$NORMAL"
+        printf '%s%s%*s%s\n' "$1" "$GREEN" $((COL-${#1})) "[OK]" "$NORMAL"
     else
-        printf '%s%s%*s%s\n' "$1" "$RED" $((col-${#1})) "[FAILED]" "$NORMAL"
+        printf '%s%s%*s%s\n' "$1" "$RED" $((COL-${#1})) "[FAILED]" "$NORMAL"
     fi
 else
     if [ -s "$ERR" ]; then
-        printf '%s%s%*s%s\n' "$1" "$RED" $((col-${#1})) "[FAILED]" "$NORMAL"
+        printf '%s%s%*s%s\n' "$1" "$RED" $((COL-${#1})) "[FAILED]" "$NORMAL"
         exit 2
     fi
-    $CPP $OLEVEL "${1%.out}.ll" $RUNTIME -o "${1%.out}.bin" -Wl,--whole-archive -static -lrt -Wl,--whole-archive -lpthread -Wl,--no-whole-archive
-    "${1%.out}.bin" > $1
-    diff -B -w $1 $EXP > /dev/null
+    $CPP $OLEVEL "${1%.test}.ll" $RUNTIME -o "${1%.test}.bin" -Wl,--whole-archive -static -lrt -Wl,--whole-archive -lpthread -Wl,--no-whole-archive
+    "${1%.test}.bin" > "${1%.test}.out"
+    diff -B -w "${1%.test}.out" $EXP > /dev/null
     if [ $? -eq 0 ]; then
         touch $1
-        printf '%s%s%*s%s\n' "$1" "$GREEN" $((col-${#1})) "[OK]" "$NORMAL"
+        printf '%s%s%*s%s\n' "$1" "$GREEN" $((COL-${#1})) "[OK]" "$NORMAL"
     else
-        printf '%s%s%*s%s\n' "$1" "$RED" $((col-${#1})) "[FAILED]" "$NORMAL"
+        printf '%s%s%*s%s\n' "$1" "$RED" $((COL-${#1})) "[FAILED]" "$NORMAL"
     fi
 fi
-
