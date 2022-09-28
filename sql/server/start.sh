bd690ddc15a0886cc142c75c0baafaa65775ad2b
julien verlaguet <julien.verlaguet@gmail.com>
2022-09-28 10:20:43 +0000

    fixed used-indexes typo

--- sql/server/start.sh
+++ sql/server/start.sh
@@ -11,17 +11,30 @@ $SKDB_BIN --init $FILE
 
 cat ../privacy/init.sql | $SKDB
 
-echo "insert into skdb_users values(id(), 'julienv');"
-echo "insert into skdb_users values(id(), 'daniell');"
-echo "insert into skdb_users values(id(), 'gregs');"
-echo "insert into skdb_users values(id(), 'lucash');"
-echo "insert into skdb_users values(id(), 'laurem');"
+echo "create table skiplabs_employees (userID INTEGER);"  | $SKDB
+
+echo "insert into skdb_user values(id(), 'stranger');" | $SKDB
+
+(echo "begin transaction;"
+ echo "insert into skdb_users values(id('julienv'), 'julienv');" 
+ echo "insert into skdb_users values(id('daniell'), 'daniell');" 
+ echo "insert into skdb_users values(id('gregs'), 'gregs');" 
+ echo "insert into skdb_users values(id('lucash'), 'lucash');"
+ echo "insert into skdb_users values(id('laurem'), 'laurem');"
+
+ echo "insert into skiplabs_employees values(id('julienv'));" 
+ echo "insert into skiplabs_employees values(id('daniell'));" 
+ echo "insert into skiplabs_employees values(id('gregs'));" 
+ echo "insert into skiplabs_employees values(id('lucash')');"
+ echo "insert into skiplabs_employees values(id('laurem')');"
+ echo "commit;"
+) | $SKDB
+
+
+
 
 echo "create table posts (sessionID integer, localID integer, skdb_privacy integer, skdb_owner integer, data string);" | $SKDB
 
-GROUPID=`echo "select groupID from skdb_groups where readers = 'whitelist_group2';" | $SKDB`
-
-echo "insert into posts values (id(), id(), $GROUPID, 6, 'The first post!');" | $SKDB
 
 
 cd ~/websockify && ./run 3048 -- /home/julienv/skfs/sql/server/start_tcp_server.sh
