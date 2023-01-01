2929068088dd7507f9b5d39f1b37b4897c65c790
julien verlaguet <julien.verlaguet@gmail.com>
2023-01-01 17:24:30 +0000

    Support for BOOLEAN
    This is just syntatic sugar:
      - BOOLEAN/boolean is equivalent to INTEGER
      - TRUE/true is equivalent to 1
      - FALSE/false is equivalent to 0

---
+++ sql/js/run_tests.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+skdb=../../build/skdb
+
+run_compare_test () {
+  echo -en "$1:\t"
+  tmpfile1=$(mktemp /tmp/testfile.XXXXXX)
+  tmpfile2=$(mktemp /tmp/testfile.XXXXXX)
+  cat ../test/$1.sql | $skdb --always-allow-joins > $tmpfile1
+  (cd ../../ && cat build/skdb_node.js sql/js/tests/$1.jsx | node) | egrep -v '^[ ]*$' > $tmpfile2
+  diff $tmpfile1 $tmpfile2 > /dev/null
+  if [ $? -eq 0 ]; then
+      rm $tmpfile1 $tmpfile2
+      echo "OK"
+  else
+      echo "FAILED"
+  fi
+}
+
+run_compare_test select1
+
+if
+    (cd ../../ && (
+        cat build/skdb_node.js;
+        echo "async function test() {"
+        echo "  skdb = await SKDB.create(true);"
+        echo "  skdb.sqlRaw('create table t1 (a BOOLEAN, b boolean);');"
+        echo "  skdb.sqlRaw('insert into t1 values(TRUE, false);');"
+        echo "  console.log(skdb.sqlRaw('select true, false, a, b from t1;'));"
+        echo "}"
+        echo "test()"
+    ) | node) | grep -q '1|0|1|0'
+then
+    echo -e "TEST JS BOOLEAN:\tOK"
+else
+    echo -e "TEST JS BOOLEAN:\tFAILED"
+fi
