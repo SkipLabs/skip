7376748cf0b2c0062a72701585d04473a7178683
julien verlaguet <julien.verlaguet@gmail.com>
2023-02-07 14:15:35 +0000

    Support for tableName.* construction

---
+++ sql/test/test_table_dot_star.sql
@@ -0,0 +1,6 @@
+create table t1 (a INTEGER, b INTEGER, c INTEGER);
+create table t2 (a INTEGER, b INTEGER);
+insert into t1 values (2, 3, 4);
+insert into t2 values (2, 6);
+
+select t1.*, t1.a+t1.b, t3.* from t1, t2 as t3 where t1.a = t2.a;
