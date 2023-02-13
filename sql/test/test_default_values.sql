d6ab25b0842d20c32d0198681cbdc195482a3782
julien verlaguet <julien.verlaguet@gmail.com>
2023-02-13 15:30:36 +0000

    Allow unique/primary/not null/default in any order
    The  parser was forcing an order on type qualifiers such as unique
    primary etc ... This diff removes this constraint.

--- sql/test/test_default_values.sql
+++ sql/test/test_default_values.sql
@@ -1,4 +1,6 @@
 create table t1 (a INTEGER PRIMARY KEY, b STRING, c STRING default 'hello');
+create table t2 (a INTEGER PRIMARY KEY, b STRING, c STRING not null default 'hello');
+create table t3 (a INTEGER PRIMARY KEY, b STRING, c STRING default 'hello' not null);
 insert into t1 (b, c) values ('test1', 'test2');
 insert into t1 (a, c) values (id(), 'foo');
 insert into t1 (a, b) values (id(), 'bar');
