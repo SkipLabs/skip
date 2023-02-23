babb11639dd6127adcc2f7e3b88dea6496abd1c3
Greg Sexton <gregsexton@gmail.com>
2023-02-23 12:28:55 +0000

    start.sh didn't support zero args

--- sql/server/deploy/start.sh
+++ sql/server/deploy/start.sh
@@ -7,5 +7,11 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source "/root/.sdkman/bin/sdkman-init.sh"
+
 cd "$SCRIPT_DIR/../skgw" || exit 1
-exec gradle --console plain run "--args=$*"
+
+if [[ $# -gt 0 ]]; then
+    exec gradle --console plain run "--args=$*"
+else
+    exec gradle --console plain run
+fi
