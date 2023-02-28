d0e50e08949e824931332e45f36b7b854b0848a6
Greg Sexton <gregsexton@gmail.com>
2023-02-28 16:09:46 +0000

    Downgrade fake-indexeddb
    This is to work around a heap growth issue

--- sql/node/src/node_header.js
+++ sql/node/src/node_header.js
@@ -4,5 +4,4 @@ const fs = require('fs');
 let FileReader = require('filereader');
 let readline = require('readline');
 const WebSocket = require('ws');
-const IndexedDB = require('fake-indexeddb');
-const indexedDB = IndexedDB.indexedDB;
+const indexedDB = require('fake-indexeddb');
