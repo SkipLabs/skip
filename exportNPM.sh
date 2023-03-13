0545527d8d3a0a1bab949cbede7bf5769557f4ca
julien verlaguet <julien.verlaguet@gmail.com>
2023-03-13 13:18:45 +0000

    Removed tsconfig.json from exportNPM.sh
    Turns out it's not needed. I originally thought that it would be
    a good idea to copy over the options used to compiler type-script
    into the NPM package. But apparently that's not expected.

--- exportNPM.sh
+++ exportNPM.sh
@@ -3,4 +3,4 @@
 cp sql/js/package.json $1/packages/skdb/package.json
 cp sql/js/skdb.wasm $1/packages/skdb/skdb.wasm
 cp sql/js/src/skdb.ts $1/packages/skdb/src/skdb-browser.ts
-cp sql/js/tsconfig.json $1/packages/skdb/tsconfig.json
+
