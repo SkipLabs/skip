21c86abe442e95498cd99b0378d8369786a74694
Greg Sexton <gregsexton@gmail.com>
2023-03-22 10:01:18 +0000

    Containerize nginx
    A deployable image is easier to manage and allows for easy local
    testing.

--- sql/server/deploy/push-build.sh
+++ sql/server/deploy/push-build.sh
@@ -1,4 +1,4 @@
-#! /bin/zsh
+#! /bin/bash
 
 set -x
 set -e
@@ -8,18 +8,40 @@ set -e
 # it's hideous. it will only work for gds and relies on his personal setup and access keys.
 # but better than no docs at all, right?
 
-cd ~/code/skfs
+cd ~/code/skfs || exit 1
 
-docker build -t deploy --progress=plain -f sql/server/deploy/Dockerfile .
+echo "Building skgw image"
+docker build -t skgw-deploy --progress=plain -f sql/server/deploy/Dockerfile .
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 400962513797.dkr.ecr.us-east-1.amazonaws.com
-docker tag deploy:latest 400962513797.dkr.ecr.us-east-1.amazonaws.com/skdb:latest
+cd ~/code/skfs/sql/server/proxy || exit 1
+
+echo "Building nginx image"
+docker build -t nginx-deploy --progress=plain -f Dockerfile .
+
+echo "Logging docker in to AWS repo"
+aws ecr get-login-password --region us-east-1 |
+    docker login --username AWS --password-stdin 400962513797.dkr.ecr.us-east-1.amazonaws.com
+
+echo "Pushing skgw"
+docker tag skgw-deploy:latest 400962513797.dkr.ecr.us-east-1.amazonaws.com/skdb:latest
 docker push 400962513797.dkr.ecr.us-east-1.amazonaws.com/skdb:latest
+
+echo "Pushing nginx"
+docker tag nginx-deploy:latest 400962513797.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
+docker push 400962513797.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
 
 # pulling down and running the container is entirely manual right now. I'm not even scripting it.
 # but the steps are:
+
 # ssh corp # get on to the bastion
+
+# aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 400962513797.dkr.ecr.us-east-1.amazonaws.com
+# sudo /usr/bin/docker pull 400962513797.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
+# sudo docker stop $(sudo docker ps -q)
+# sudo /usr/bin/docker run -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt 400962513797.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
+
 # ssh -i ~/.ssh/gds_id_ed25519 ubuntu@10.0.131.45 # get on to the replication server
+
 # aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 400962513797.dkr.ecr.us-east-1.amazonaws.com
 # sudo /usr/bin/docker pull 400962513797.dkr.ecr.us-east-1.amazonaws.com/skdb:latest
 # sudo docker stop $(sudo docker ps -q)
