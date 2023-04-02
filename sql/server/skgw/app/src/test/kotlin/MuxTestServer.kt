8e10546a907abf0e42f2ef3f1096a6584bd5d115
Greg Sexton <gregsexton@gmail.com>
2023-04-02 16:45:41 +0100

    Lay down an initial testing protcol for end-to-end integration tests

--- sql/server/skgw/app/src/test/kotlin/MuxTestServer.kt
+++ sql/server/skgw/app/src/test/kotlin/MuxTestServer.kt
@@ -3,12 +3,67 @@ package io.skiplabs.skgw.test
 import io.skiplabs.skgw.MuxedSocket
 import io.skiplabs.skgw.MuxedSocketEndpoint
 import io.skiplabs.skgw.MuxedSocketFactory
+import io.skiplabs.skgw.Stream
 import io.undertow.Handlers
 import io.undertow.Undertow
 import io.undertow.server.handlers.PathTemplateHandler
 import io.undertow.websockets.core.WebSocketChannel
 import io.undertow.websockets.spi.WebSocketHttpExchange
+import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
+
+val streams: List<Stream> = listOf()
+var lastSocket: MuxedSocket? = null
+
+fun setupStream(sock: MuxedSocket, s: Stream) {
+  lastSocket = sock
+
+  s.onData = { data ->
+    val type = data.getInt()
+    when (type) {
+      // close from this side
+      0 -> {
+        s.close()
+      }
+      // echo
+      1 -> {
+        val n = data.getInt()
+        val buf = ByteBuffer.allocate(4)
+        buf.putInt(n)
+        buf.flip()
+        s.send(buf)
+      }
+      // send n messages
+      2 -> {
+        val n = data.getInt()
+        for (i in 0..n) {
+          val buf = ByteBuffer.allocate(4)
+          buf.putInt(i)
+          buf.flip()
+          s.send(buf)
+        }
+      }
+      // open a stream
+      3 -> {
+        val newStream = sock.openStream()
+        if (newStream == null) {} else {
+          setupStream(sock, newStream)
+        }
+        val buf = ByteBuffer.allocate(4)
+        buf.putInt(0xABCDEF12u.toInt())
+        buf.flip()
+        newStream?.send(buf)
+      }
+      // error from this side
+      4 -> {
+        s.error(5u, "server errored stream")
+      }
+      else -> throw RuntimeException("oops")
+    }
+  }
+  s.onClose = { s.close() }
+  s.onError = { code, reason -> }
+}
 
 fun createHttpServer(): Undertow {
   val muxHandler =
@@ -19,12 +74,14 @@ fun createHttpServer(): Undertow {
                     exchange: WebSocketHttpExchange,
                     channel: WebSocketChannel
                 ): MuxedSocket {
-                  return MuxedSocket(
-                      onStream = { stream -> println("Got a stream: ${stream}") },
-                      onClose = { println("Closing socket") },
-                      onError = { code, msg -> println("Socket errored [${code}]: ${msg}") },
-                      socket = channel,
-                      getDecryptedKey = { "test".toByteArray(StandardCharsets.UTF_8) })
+                  val socket =
+                      MuxedSocket(
+                          onStream = ::setupStream,
+                          onClose = {},
+                          onError = { socket, code, msg -> },
+                          socket = channel,
+                          getDecryptedKey = { "test".toByteArray(StandardCharsets.UTF_8) })
+                  return socket
                 }
               }))
 
