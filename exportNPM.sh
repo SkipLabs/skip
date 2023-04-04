fde3bad1a0e2b61323bf5d30a1e5e57b3aa35a3b
Greg Sexton <gregsexton@gmail.com>
2023-04-04 19:59:51 +0100

    Update export script for node and cli

--- exportNPM.sh
+++ exportNPM.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-cp sql/js/package.json $1/packages/skdb/package.json
-cp sql/js/skdb.wasm $1/packages/skdb/skdb.wasm
-cp sql/js/src/skdb.ts $1/packages/skdb/src/skdb-browser.ts
+if [[ "$#" -ne 1 ]];
+then
+    echo "Pass the path to your public skdb repo as an argument" >&2
+    exit 1
+fi
 
+PUBLIC_SKDB_REPO=$1
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR" || exit 1
+
+cp sql/js/package.json "$PUBLIC_SKDB_REPO"/packages/skdb/package.json
+cp sql/js/tsconfig.json "$PUBLIC_SKDB_REPO"/packages/skdb/tsconfig.json
+
+cp sql/js/skdb.wasm "$PUBLIC_SKDB_REPO"/packages/skdb/skdb.wasm
+
+cp sql/js/src/skdb.ts "$PUBLIC_SKDB_REPO"/packages/skdb/src/skdb-browser.ts
+cp sql/js/src/skdb-cli.js "$PUBLIC_SKDB_REPO"/packages/skdb/src/skdb-cli.js
+
+# produced by makefile
+cp sql/js/dist/skdb-node.js "$PUBLIC_SKDB_REPO"/packages/skdb/src/skdb-node.js
