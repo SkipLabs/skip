d75805a534ff46279957d2d8936ea90129163f44
Greg Sexton <gregsexton@gmail.com>
2023-04-05 14:25:55 +0100

    Get js tests running again

--- sql/js/run_tests.sh
+++ sql/js/run_tests.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 skdb=../../build/skdb
-node=./node_modules/node/bin/node
+node="./node_modules/node/bin/node --input-type=module"
+skdb_node=./dist/skdb-node.js
 
 run_compare_test () {
   echo -en "$1:\t"
   tmpfile1=$(mktemp /tmp/testfile.XXXXXX)
   tmpfile2=$(mktemp /tmp/testfile.XXXXXX)
   cat ../test/$1.sql | $skdb --always-allow-joins > $tmpfile1
-  ((cat ../../build/skdb_node.js; echo ""; cat tests/$1.jsx) | $node) | egrep -v '^[ ]*$' > $tmpfile2
+  ((cat $skdb_node; echo ""; cat tests/$1.jsx) | $node) | egrep -v '^[ ]*$' > $tmpfile2
   diff $tmpfile1 $tmpfile2 > /dev/null
   if [ $? -eq 0 ]; then
       rm $tmpfile1 $tmpfile2
@@ -21,7 +22,7 @@ run_compare_test () {
 run_compare_test select1
 
 if
-    ((cat ../../build/skdb_node.js;
+    ((cat $skdb_node;
         echo "";
         echo "async function test() {"
         echo "  let skdb = await SKDB.create(true);"
@@ -39,10 +40,10 @@ fi
 
 if
     ((
-        cat ../../build/skdb_node.js;
+        cat $skdb_node;
         echo "";
         echo "async function test() {"
-        echo "  skdb = await SKDB.create(true);"
+        echo "  let skdb = await SKDB.create(true);"
         echo "  skdb.sqlRaw('create table t1 (a BOOLEAN, b boolean);');"
         echo "  skdb.sqlRaw('create table if not exists t1 (a BOOLEAN, b boolean);');"
         echo "  console.log(skdb.sqlRaw('select 1;'));"
@@ -57,10 +58,10 @@ fi
 
 if
     ((
-        cat ../../build/skdb_node.js;
+        cat $skdb_node;
         echo "";
         echo "async function test() {"
-        echo "  skdb = await SKDB.create(true);"
+        echo "  let skdb = await SKDB.create(true);"
         echo "  skdb.sqlRaw('create table t1 (a STRING PRIMARY KEY);');"
         echo "}"
         echo "test()"
@@ -73,10 +74,10 @@ fi
 
 if
     ((
-        cat ../../build/skdb_node.js;
+        cat $skdb_node;
         echo "";
         echo "async function test() {"
-        echo "  skdb = await SKDB.create(true);"
+        echo "  let skdb = await SKDB.create(true);"
         echo "  skdb.sqlRaw('create table t1 (a STRING PRIMARY KEY, b INTEGER);');"
         echo "  skdb.sqlRaw('insert into t1 (b) values (22);');";
         echo "}"
@@ -90,10 +91,10 @@ fi
 
 if
     ((
-        cat ../../build/skdb_node.js;
+        cat $skdb_node;
         echo "";
         echo "async function test() {"
-        echo "  skdb = await SKDB.create(true);"
+        echo "  let skdb = await SKDB.create(true);"
         echo "  skdb.sql('create table widgets (id text unique, name text);');"
         echo "  skdb.sql('INSERT INTO widgets (id, name) VALUES (\'a\', \'gear\');');"
         echo "  skdb.sql('UPDATE widgets SET id = \'c\', name = \'gear2\';');"
@@ -109,10 +110,10 @@ fi
 
 if
     ((
-        cat ../../build/skdb_node.js;
+        cat $skdb_node;
         echo "";
         echo "async function test() {"
-        echo "  skdb = await SKDB.create(true);"
+        echo "  let skdb = await SKDB.create(true);"
         echo "  skdb.sql(\"create table widgets (id text unique , price real not null);\");"
         echo "  skdb.sql(\"INSERT INTO widgets (id, price) values ('a', 10.0);\");"
         echo "  console.log(skdb.sqlRaw('select * from widgets'))"
@@ -127,10 +128,10 @@ fi
 
 if
     ((
-        cat ../../build/skdb_node.js;
+        cat $skdb_node;
         echo "";
         echo "async function test() {"
-        echo "  skdb = await SKDB.create(true);"
+        echo "  let skdb = await SKDB.create(true);"
         echo "  skdb.sqlRaw('create table t1 (a BOOLEAN, b boolean);');"
         echo "  skdb.sqlRaw('create virtual view v1 as select * from t1;');"
         echo "  skdb.sqlRaw('create virtual view if not exists v1 as select * from t1;');"
@@ -146,10 +147,10 @@ fi
 
 if
     ((
-        cat ../../build/skdb_node.js;
+        cat $skdb_node;
         echo "";
         echo "async function test() {"
-        echo "  skdb = await SKDB.create(true);"
+        echo "  let skdb = await SKDB.create(true);"
         echo "  skdb.sqlRaw('create table t1 (a BOOLEAN, b boolean);');"
         echo "  skdb.sqlRaw('create view v1 as select * from t1;');"
         echo "  skdb.sqlRaw('create view if not exists v1 as select * from t1;');"
@@ -165,10 +166,10 @@ fi
 
 if
     ((
-        cat ../../build/skdb_node.js;
+        cat $skdb_node;
         echo "";
         echo "async function test() {"
-        echo "  skdb = await SKDB.create(true);"
+        echo "  let skdb = await SKDB.create(true);"
         echo "  for(let i = 0; i < 10000; i++) {"
         echo "    skdb.sqlRaw('select * from t1;');"
         echo "    console.log('');"
@@ -184,10 +185,10 @@ fi
 
 if
     ((
-        cat ../../build/skdb_node.js;
+        cat $skdb_node;
         echo "";
         echo "async function test() {"
-        echo "  skdb = await SKDB.create(true);"
+        echo "  let skdb = await SKDB.create(true);"
         echo "  skdb.sql('create table t1 (aBc INTEGER)')"
         echo "  skdb.insert('t1', [11]);"
         echo "  console.log(skdb.sql('select * from t1')[0].aBc);"
@@ -202,7 +203,7 @@ fi
 
 if
     ((
-        cat ../../build/skdb_node.js;
+        cat $skdb_node;
         cat tests/test_root.js
     ) | $node 2>&1) | tr '\n' 'S' | grep -q 'todos \[\]S'
 then
--- sql/js/tests/test_root.js
+++ sql/js/tests/test_root.js
@@ -1,24 +1,22 @@
+
 async function test() {
   let skdb = await SKDB.create(true);
-  skdb.sqlRaw('create table if not exists todos (id text primary key, text text, completed integer);')
+  skdb.sqlRaw('create table if not exists todos (id text primary key, text text, completed integer);');
 
-  const ROOT_ID = 'todosRoot'
+  const ROOT_ID = 'todosRoot';
 
   // Make a callable that returns the SQL query string we want to run
-  const queryStringCallable = skdb.registerFun(() => `select * from todos`)
-  
+  const queryStringCallable = skdb.registerFun(() => `select * from todos`);
+
   // Make a tracked function which fetches the query string and then runs the query
   const todos = skdb.registerFun(() => {
-    const queryString = skdb.trackedCall(queryStringCallable, null)
-    return skdb.trackedQuery(queryString)
-  })
+    const queryString = skdb.trackedCall(queryStringCallable, null);
+    return skdb.trackedQuery(queryString);
+  });
 
   // Add a root for the tracked function and get its result
-  skdb.addRoot(ROOT_ID, todos, null)
+  skdb.addRoot(ROOT_ID, todos, null);
   console.log('todos', skdb.getRoot(ROOT_ID));
 }
 
 test();
-
-
-
