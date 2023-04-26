c56a1d9d6091510ee8b080153a5cba57e3651b36
Greg Sexton <gregsexton@gmail.com>
2023-04-26 10:36:18 +0100

    Remove unneeded FileReader dep

--- sql/js/src/node_header.js
+++ sql/js/src/node_header.js
@@ -2,6 +2,5 @@ import { webcrypto as crypto } from 'node:crypto';
 import 'node:util';
 import fs from 'node:fs';
 import 'readline';
-import FileReader from 'filereader';
 import { WebSocket } from 'ws';
 import indexedDB from 'fake-indexeddb';
