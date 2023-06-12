46ba10120f0cb7ddab92bacf82c0716475e4d262
Josh Berdine <josh@skiplabs.io>
2023-06-12 15:33:55 +0100

    [skdb] Align float printing with sqlite
    Revise printing of float values to align with sqlite. In particular
    this involves:
    
      - avoiding "e" notation when the exponent would be between -4 and 14
    
      - zero-padding exponents to ensure they have at least 2 digits
    
      - including `.0` even if there is an exponent
    
    This PR adds a few additional test cases to exercise either side of
    the format thresholds.
    
    The "parse float" unit test now passes.

--- sql/test/unit_parse_float.sql
+++ sql/test/unit_parse_float.sql
@@ -1,12 +1,25 @@
 create table t1(f FLOAT);
 
+select 1000000000.0;
+select 10000000000.0;
 select 100000000000000.0;
+select 1000000000000000.0;
+select 0.00001;
+select 0.000011;
+select 0.00009;
+select 0.0001;
+select 0.0009;
 select 58.;
 select 4e2;
 select 123.456e-67;
 select 0.1E4;
 
 select -100000000000000.0;
+select -1000000000000000.0;
+select -0.0001;
+select -0.00001;
+select -0.000000001;
+select -0.0000000001;
 select -58.;
 select -4e2;
 select -123.456e-67;
@@ -14,6 +27,14 @@ select -0.1E4;
 
 
 insert into t1 values(100000000000000.0);
+insert into t1 values(1000000000000000.0);
+insert into t1 values(0.00001);
+insert into t1 values(0.000011);
+insert into t1 values(0.00009);
+insert into t1 values(0.0001);
+insert into t1 values(0.0009);
+insert into t1 values(0.0001);
+insert into t1 values(0.00001);
 insert into t1 values(58.);
 insert into t1 values(4e2);
 insert into t1 values(123.456e-67);
@@ -21,10 +42,15 @@ insert into t1 values(0.1E4);
 
 
 insert into t1 values(-100000000000000.0);
+insert into t1 values(-1000000000000000.0);
+insert into t1 values(-0.0001);
+insert into t1 values(-0.00001);
+insert into t1 values(-0.00000001);
+insert into t1 values(-0.000000001);
 insert into t1 values(-58.);
 insert into t1 values(-4e2);
 insert into t1 values(-123.456e-67);
 insert into t1 values(-0.1E4);
 
 
-select * from t1;
+select * from t1 order by f desc;
