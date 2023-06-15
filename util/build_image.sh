ca67c93dfb840df906ca52359e26224d82965693
Josh Berdine <josh@skiplabs.io>
2023-06-15 14:50:20 +0100

    Add a script to build the docker images

---
+++ util/build_image.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO="$SCRIPT_DIR/.."
+
+set -e
+set -x
+
+cd $REPO
+
+git clean -xdn | sed 's|Would remove |/|g' > .dockerignore
+
+docker build --no-cache --tag skfs --progress=plain .
+docker build --no-cache --tag skgw --progress=plain --file sql/server/skgw/Dockerfile .
+docker build --tag $USER-skfs --progress=plain --file $USER/Dockerfile .
+
+git restore .dockerignore
+
+echo disk usage:
+docker system df
+
+echo images:
+docker images
