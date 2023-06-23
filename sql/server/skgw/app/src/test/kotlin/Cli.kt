45254077c5f116fcc345188ecb68bda94ab719b2
Greg Sexton <gregsexton@gmail.com>
2023-06-23 15:41:30 +0100

    Implement sqlRaw
    Generalise req/resp while here.

--- sql/server/skgw/app/src/test/kotlin/Cli.kt
+++ sql/server/skgw/app/src/test/kotlin/Cli.kt
@@ -1,13 +1,14 @@
 package io.skiplabs.skgw.cli
 
-import kotlinx.coroutines.runBlocking
-import io.skiplabs.skgw.client.SkdbConnection
 import io.skiplabs.skgw.Credentials
+import io.skiplabs.skgw.client.SkdbConnection
 import io.skiplabs.skgw.openSkdb
 import java.net.URI
+import java.util.Base64
 import java.util.UUID
 import java.util.concurrent.Executors
-import java.util.Base64
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
   val pool = Executors.newSingleThreadScheduledExecutor()
@@ -19,19 +20,36 @@ fun main() = runBlocking {
   val accessKey = "root"
   val privateKey = Base64.getDecoder().decode("kNqxI8l+1Y1rS2i3M9dOONBTdUQXYsttby7bxD3rhpo=")
 
-  val conn = SkdbConnection(
-    endpoint = URI("${endpoint}/dbs/${db}/connection"),
-    creds = Credentials(
-      accessKey = accessKey,
-      privateKey = privateKey,
-      encryptedPrivateKey = ByteArray(0),
-      deviceUuid = UUID.randomUUID().toString(),
-    ),
-    local = skdb!!,
-    taskPool = pool,
-  )
+  val conn =
+      SkdbConnection(
+          endpoint = URI("${endpoint}/dbs/${db}/connection"),
+          creds =
+              Credentials(
+                  accessKey = accessKey,
+                  privateKey = privateKey,
+                  encryptedPrivateKey = ByteArray(0),
+                  deviceUuid = UUID.randomUUID().toString(),
+              ),
+          local = skdb!!,
+          taskPool = pool,
+      )
 
   conn.open()
-  val schema = conn.schema()
-  println(": [hsexs] schema: ${schema}")
+
+  val work = launch {
+    launch {
+      val create = conn.sqlRaw("CREATE TABLE IF NOT EXISTS test (x INTEGER PRIMARY KEY, y STRING);")
+      println(": [ggyxt] create: ${create}")
+      val result = conn.sqlRaw("SELECT * FROM test")
+      println(": [xcvtf] result: ${result}")
+    }
+
+    launch {
+      val schema = conn.schema()
+      println(": [hsexs] schema: ${schema}")
+    }
+  }
+
+  work.join()
+  conn.close()
 }
