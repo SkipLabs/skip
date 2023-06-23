da0df26385925665175742b92f32c61154b838ef
Greg Sexton <gregsexton@gmail.com>
2023-06-23 16:54:07 +0100

    Basic remote tailing to local table works

--- sql/server/skgw/app/src/main/kotlin/Orchestration.kt
+++ sql/server/skgw/app/src/main/kotlin/Orchestration.kt
@@ -151,11 +151,12 @@ fun encodeProtoMsg(msg: ProtoMessage): ByteBuffer {
     is ProtoSchemaQuery -> {
       val buf = ByteBuffer.allocate(4 + (msg.name?.length ?: 0) * 4)
       buf.put(0x04.toByte())
-      buf.put(when (msg.scope) {
-        SchemaScope.ALL -> 0x0
-        SchemaScope.TABLE -> 0x1
-        SchemaScope.VIEW -> 0x2
-      })
+      buf.put(
+          when (msg.scope) {
+            SchemaScope.ALL -> 0x0
+            SchemaScope.TABLE -> 0x1
+            SchemaScope.VIEW -> 0x2
+          })
       val pos = buf.position()
       buf.putShort(0x0)
       val encoder = StandardCharsets.UTF_8.newEncoder()
@@ -173,11 +174,12 @@ fun encodeProtoMsg(msg: ProtoMessage): ByteBuffer {
     is ProtoQuery -> {
       val buf = ByteBuffer.allocate(6 + msg.query.length * 4)
       buf.put(0x01.toByte())
-      buf.put(when (msg.format) {
-        QueryResponseFormat.JSON -> 0x0
-        QueryResponseFormat.RAW -> 0x1
-        QueryResponseFormat.CSV -> 0x2
-      })
+      buf.put(
+          when (msg.format) {
+            QueryResponseFormat.JSON -> 0x0
+            QueryResponseFormat.RAW -> 0x1
+            QueryResponseFormat.CSV -> 0x2
+          })
       val pos = buf.position()
       buf.putInt(0x0)
       val encoder = StandardCharsets.UTF_8.newEncoder()
@@ -190,6 +192,63 @@ fun encodeProtoMsg(msg: ProtoMessage): ByteBuffer {
         res.throwException()
       }
       buf.putInt(pos, (buf.position() - pos - 4))
+      buf.flip()
+    }
+    is ProtoRequestTail -> {
+      val buf = ByteBuffer.allocate(16 + msg.table.length * 4 + (msg.filterExpr?.length ?: 0) * 4)
+      buf.putInt(0x0)
+      buf.put(0, 0x02.toByte())
+      buf.putLong(msg.since.toLong())
+
+      var pos = buf.position()
+      buf.putShort(0x0)
+      val encoder = StandardCharsets.UTF_8.newEncoder()
+      var res = encoder.encode(CharBuffer.wrap(msg.table), buf, true)
+      if (!res.isUnderflow()) {
+        res.throwException()
+      }
+      res = encoder.flush(buf)
+      if (!res.isUnderflow()) {
+        res.throwException()
+      }
+      val tableLengthEncoded = (buf.position() - pos - 2).toShort()
+      buf.putShort(pos, tableLengthEncoded)
+      encoder.reset()
+
+      if (msg.filterExpr != null) {
+        pos = buf.position()
+        buf.putShort(0x0)
+        res = encoder.encode(CharBuffer.wrap(msg.filterExpr), buf, true)
+        if (!res.isUnderflow()) {
+          res.throwException()
+        }
+        res = encoder.flush(buf)
+        if (!res.isUnderflow()) {
+          res.throwException()
+        }
+        buf.putShort(pos, (buf.position() - pos - 2).toShort())
+      }
+
+      buf.flip()
+    }
+    is ProtoPushPromise -> {
+      val buf = ByteBuffer.allocate(6 + msg.table.length * 4)
+      buf.putInt(0x0)
+      buf.put(0, 0x03.toByte())
+
+      var pos = buf.position()
+      buf.putShort(0x0)
+      val encoder = StandardCharsets.UTF_8.newEncoder()
+      var res = encoder.encode(CharBuffer.wrap(msg.table), buf, true)
+      if (!res.isUnderflow()) {
+        res.throwException()
+      }
+      res = encoder.flush(buf)
+      if (!res.isUnderflow()) {
+        res.throwException()
+      }
+      buf.putShort(pos, (buf.position() - pos - 2).toShort())
+
       buf.flip()
     }
     else -> throw RuntimeException("We don't support encoding ${msg}")
--- sql/server/skgw/app/src/test/kotlin/Cli.kt
+++ sql/server/skgw/app/src/test/kotlin/Cli.kt
@@ -1,7 +1,11 @@
 package io.skiplabs.skgw.cli
 
 import io.skiplabs.skgw.Credentials
+import io.skiplabs.skgw.NoEncryptionTransform
+import io.skiplabs.skgw.OutputFormat
 import io.skiplabs.skgw.client.SkdbConnection
+import io.skiplabs.skgw.createSkdb
+import io.skiplabs.skgw.genCredentials
 import io.skiplabs.skgw.openSkdb
 import java.net.URI
 import java.util.Base64
@@ -13,24 +17,30 @@ import kotlinx.coroutines.runBlocking
 fun main() = runBlocking {
   val pool = Executors.newSingleThreadScheduledExecutor()
 
-  val skdb = openSkdb("client")
+  val localDbName = "client"
+  val localUser = "root"
+  val skdb =
+      openSkdb(localDbName)
+          ?: createSkdb(
+              localDbName, genCredentials(localUser, NoEncryptionTransform()).b64encryptedKey())
+  skdb.sql("CREATE TABLE IF NOT EXISTS test (x INTEGER PRIMARY KEY, y STRING);", OutputFormat.RAW)
 
   val endpoint = "ws://localhost:8080"
   val db = "foo"
-  val accessKey = "root"
-  val privateKey = Base64.getDecoder().decode("kNqxI8l+1Y1rS2i3M9dOONBTdUQXYsttby7bxD3rhpo=")
+  val remoteUser = "root"
+  val remotePrivateKey = Base64.getDecoder().decode("kNqxI8l+1Y1rS2i3M9dOONBTdUQXYsttby7bxD3rhpo=")
 
   val conn =
       SkdbConnection(
           endpoint = URI("${endpoint}/dbs/${db}/connection"),
           creds =
               Credentials(
-                  accessKey = accessKey,
-                  privateKey = privateKey,
+                  accessKey = remoteUser,
+                  privateKey = remotePrivateKey,
                   encryptedPrivateKey = ByteArray(0),
                   deviceUuid = UUID.randomUUID().toString(),
               ),
-          local = skdb!!,
+          local = skdb,
           taskPool = pool,
       )
 
@@ -40,8 +50,13 @@ fun main() = runBlocking {
     launch {
       val create = conn.sqlRaw("CREATE TABLE IF NOT EXISTS test (x INTEGER PRIMARY KEY, y STRING);")
       println(": [ggyxt] create: ${create}")
-      val result = conn.sqlRaw("SELECT * FROM test")
-      println(": [xcvtf] result: ${result}")
+
+      launch {
+        val result = conn.sqlRaw("SELECT * FROM test")
+        println(": [xcvtf] result: ${result}")
+      }
+
+      launch { conn.establishServerTail("test") }
     }
 
     launch {
