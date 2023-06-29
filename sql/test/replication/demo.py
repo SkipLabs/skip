4cdb366baa4222dd153640b287564ef81f57e462
Greg Sexton <gregsexton@gmail.com>
2023-06-29 15:52:39 +0100

    Choose a random set of schedules

--- sql/test/replication/demo.py
+++ sql/test/replication/demo.py
@@ -1,7 +1,7 @@
 import scheduling as sched
 from model import Topology, Client, Server
 
-scheduler = sched.AllTopoSortsScheduler()
+scheduler = sched.RandomTopoSortsScheduler(sched.AllTopoSortsScheduler(), 50)
 
 cluster = (
   Topology(scheduler)
