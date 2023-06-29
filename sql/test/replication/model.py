7c3d796578cbf4d1c39f579c2872ba60242fed51
Greg Sexton <gregsexton@gmail.com>
2023-06-29 14:55:51 +0100

    Split out some files

--- sql/test/replication/model.py
+++ sql/test/replication/model.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from collections import defaultdict
-import copy
 
 class HalfStream:
   buf = []
@@ -112,98 +111,6 @@ class Client(SkdbPeer):
     self.lastTask = init
     return init
 
-class Scheduler:
-  tasks = set()
-  graph = defaultdict(list)
-
-  def add(self, task):
-    self.tasks.add(task)
-
-  def happensBefore(self, a, b):
-    self.graph[a].append(b)
-
-  def run(self):
-    raise RuntimeError("scheduler is abstract")
-
-class ArbitraryTopoSortScheduler(Scheduler):
-  def run(self):
-    # kahn's algo, we pick from multiple choices arbitrarily
-
-    g = copy.deepcopy(self.graph)
-
-    def nodesWithNoIncomingEdge():
-      acc = set()
-      for node in self.tasks:
-        found = False
-        for (_, nodes) in g.items():
-          if node in nodes:
-            found = True
-            break
-        if not found:
-          acc.add(node)
-      return acc
-
-    schedule = []
-    candidates = nodesWithNoIncomingEdge()
-    while candidates != set():
-      n = candidates.pop()
-      schedule.append(n)
-      g[n] = list()
-      candidates = candidates.union(nodesWithNoIncomingEdge()).difference(set(schedule))
-
-    def hasEdges(g):
-      any(x != list() for x in g.values())
-
-    if hasEdges(g):
-      raise RuntimeError("Graph had a cycle")
-
-    # TODO:
-    print("schedule:")
-    for i, s in enumerate(schedule):
-      print(f"{i}: {s}")
-
-class AllTopoSortsScheduler(Scheduler):
-  def _nodesWithNoIncomingEdge(self, g):
-    acc = set()
-    for node in self.tasks:
-      found = False
-      for (_, nodes) in g.items():
-        if node in nodes:
-          found = True
-          break
-      if not found:
-        acc.add(node)
-    return acc
-
-  def _schedules(self, schedule, candidates, g):
-    if candidates == set():
-      if any(x != list() for x in g.values()):
-        raise RuntimeError("Graph had a cycle")
-      yield schedule
-      return
-
-    for n in candidates:
-      ourCandidates = copy.deepcopy(candidates)
-      ourSchedule = copy.deepcopy(schedule)
-      ourG = copy.deepcopy(g)
-      ourSchedule.append(n)
-      ourG[n] = list()
-      ourCandidates = ourCandidates.union(self._nodesWithNoIncomingEdge(ourG)).difference(set(ourSchedule))
-      for s in self._schedules(ourSchedule, ourCandidates, ourG):
-        yield s
-
-  def run(self):
-    schedule = []
-    candidates = self._nodesWithNoIncomingEdge(self.graph)
-    # TODO: there are more than N schedules, are you sure?
-    for i, schedule in enumerate(self._schedules(schedule, candidates, self.graph)):
-      print(f"{i}: {schedule}")
-
-class RandomTopoSortsScheduler():
-  # TODO: you will only test x% of the schedules are you aware?
-  # TODO: must be streaming really, we don't want to calculate the schedules repeatedly or keep them
-  pass
-
 class Topology:
   schemaQueries = []
   peers = []
@@ -243,29 +150,3 @@ class ResultSets():
 
   def has(self, *rows):
     pass
-
-scheduler = AllTopoSortsScheduler()
-
-cluster = (
-  Topology(scheduler)
-  .schema("CREATE TABLE test_without_pk (id INTEGER, note STRING);")
-)
-
-server = cluster.add(Server("s1", scheduler))
-client = cluster.add(Client("c1", scheduler))
-
-# TODO: cluster.mirrorAll(table)?
-cluster.mirror("test_without_pk", client, server)
-
-# TODO: how to make it clear what of these are sequential and which are concurrent?
-client.insertInto("test_without_pk", [0, 'foo'])
-client.insertInto("test_without_pk", [1, 'foo'])
-server.insertInto("test_without_pk", [2, 'foo'])
-
-cluster.eventually("SELECT * FROM test_without_pk;").has(
-  [0, "foo"], [1, "foo"], [2, "foo"]
-)
-
-# TODO: cluster.eventually().silent() - no output on processes
-
-scheduler.run()
---
+++ sql/test/replication/scheduling.py
@@ -0,0 +1,99 @@
+from collections import defaultdict
+import copy
+
+class Scheduler:
+  tasks = set()
+  graph = defaultdict(list)
+
+  def add(self, task):
+    self.tasks.add(task)
+
+  def happensBefore(self, a, b):
+    self.graph[a].append(b)
+
+  def run(self):
+    raise RuntimeError("scheduler is abstract")
+
+class ArbitraryTopoSortScheduler(Scheduler):
+  def run(self):
+    # kahn's algo, we pick from multiple choices arbitrarily
+
+    g = copy.deepcopy(self.graph)
+
+    def nodesWithNoIncomingEdge():
+      acc = set()
+      for node in self.tasks:
+        found = False
+        for (_, nodes) in g.items():
+          if node in nodes:
+            found = True
+            break
+        if not found:
+          acc.add(node)
+      return acc
+
+    schedule = []
+    candidates = nodesWithNoIncomingEdge()
+    while candidates != set():
+      n = candidates.pop()
+      schedule.append(n)
+      g[n] = list()
+      candidates = candidates.union(nodesWithNoIncomingEdge()).difference(set(schedule))
+
+    def hasEdges(g):
+      any(x != list() for x in g.values())
+
+    if hasEdges(g):
+      raise RuntimeError("Graph had a cycle")
+
+    # TODO:
+    print("schedule:")
+    for i, s in enumerate(schedule):
+      print(f"{i}: {s}")
+
+class AllTopoSortsScheduler(Scheduler):
+  def _nodesWithNoIncomingEdge(self, g):
+    acc = set()
+    for node in self.tasks:
+      found = False
+      for (_, nodes) in g.items():
+        if node in nodes:
+          found = True
+          break
+      if not found:
+        acc.add(node)
+    return acc
+
+  def _schedules(self, schedule, candidates, g):
+    if candidates == set():
+      if any(x != list() for x in g.values()):
+        raise RuntimeError("Graph had a cycle")
+      yield schedule
+      return
+
+    for n in candidates:
+      ourCandidates = copy.deepcopy(candidates)
+      ourSchedule = copy.deepcopy(schedule)
+      ourG = copy.deepcopy(g)
+      ourSchedule.append(n)
+      ourG[n] = list()
+      ourCandidates = ourCandidates.union(self._nodesWithNoIncomingEdge(ourG)).difference(set(ourSchedule))
+      for s in self._schedules(ourSchedule, ourCandidates, ourG):
+        yield s
+
+  def run(self):
+    schedule = []
+    candidates = self._nodesWithNoIncomingEdge(self.graph)
+    # TODO: there are more than N schedules, are you sure?
+    for i, schedule in enumerate(self._schedules(schedule, candidates, self.graph)):
+      print(f"{i}: {schedule}")
+
+class RandomTopoSortsScheduler():
+  # TODO: you will only test x% of the schedules are you aware?
+  # TODO: must be streaming really, we don't want to calculate the schedules repeatedly or keep them
+  pass
+
+# allows specifying sync points so that tests can ensure specific
+# schedules are exercised
+class ExtraConstraintsScheduler():
+  pass
