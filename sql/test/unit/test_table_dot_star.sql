c87fd556e6a29d6e23ba667face835382a077e28
Josh Berdine <josh@skiplabs.io>
2023-07-06 00:21:30 +0100

    Fix sql tests that fail due to misnamed columns
    These produced "no such column" errors with sqlite. It is not clear
    why skdb does not also detect these errors.

--- sql/test/unit/test_table_dot_star.sql
+++ sql/test/unit/test_table_dot_star.sql
@@ -3,4 +3,4 @@ create table t2 (a INTEGER, b INTEGER);
 insert into t1 values (2, 3, 4);
 insert into t2 values (2, 6);
 
-select t1.*, t1.a+t1.b, t3.* from t1, t2 as t3 where t1.a = t2.a;
+select t1.*, t1.a+t1.b, t3.* from t1, t2 as t3 where t1.a = t3.a;
