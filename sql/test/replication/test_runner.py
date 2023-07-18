b0c9753b3c0d16dea1a415e0e594802538eede2b
Greg Sexton <gregsexton@gmail.com>
2023-07-18 14:22:45 +0100

    Reduce chevrons
    The clutter was annoying me.

--- sql/test/replication/test_runner.py
+++ sql/test/replication/test_runner.py
@@ -6,7 +6,7 @@ import multiprocessing as mp
 
 def run_test(args):
   name, f = args
-  prefix = f">>> {name.replace('test_', '').replace('_', ' ')}......"
+  prefix = f"> {name.replace('test_', '').replace('_', ' ')}......"
   loop = asyncio.get_event_loop()
   log = []
   def collect(output):
