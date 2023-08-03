6cc33453815229cd8348a9ef2c4fc749f736d9d8
Greg Sexton <gregsexton@gmail.com>
2023-08-03 16:55:38 +0100

    Create basic full mesh scenario
    This finally demonstrates that the approach works for a fully
    connected topology.

--- sql/test/replication/test_no_pk.py
+++ sql/test/replication/test_no_pk.py
@@ -144,5 +144,32 @@ def test_two_clients_single_server_multiple_inserts_on_client1_insert_on_each():
   return scheduler
 
 
+def test_full_mesh_two_conflicting_inserts():
+  scheduler = sched.ReservoirSample(sched.AllTopoSortsScheduler(runAll=True), 5000)
+  cluster = create_cluster(scheduler)
+
+  server = cluster.add(Server("s1", scheduler))
+  client1 = cluster.add(Client("c1", scheduler))
+  client2 = cluster.add(Client("c2", scheduler))
+
+  cluster.mirror("test_without_pk", client1, server)
+  cluster.mirror("test_without_pk", client2, server)
+  cluster.mirror("test_without_pk", client1, client2)
+
+  client1.insertInto("test_without_pk", [0, 'foo'])
+  server.insertInto("test_without_pk", [0, 'foo'])
+
+  # check once all tasks have run that the cluster is silent
+  cluster.isSilent()
+
+  # and that all nodes have reached this state
+  cluster.state("SELECT id, note FROM test_without_pk;").equals(
+    [0, "foo"], [0, "foo"],
+    colnames=['id', 'note'],
+  )
+
+  return scheduler
+
+
 if __name__ == '__main__':
   run_tests(sys.modules[__name__])
