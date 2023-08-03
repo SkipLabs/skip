96d7bd3e3c450db0f4933a37e3c4575adec5518f
Lucas Hosseini <lucas@skiplabs.io>
2023-08-03 17:18:42 +0200

    [CI] Use CircleCI setup workflow to trigger relevant tests.

---
+++ .circleci/generate_config.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# TODO: This is flaky as it relies on the fork's master branch state.
+git diff --quiet HEAD master -- compiler/ prelude/ skfs/
+skc=$?
+git diff --quiet HEAD master -- skfs/
+skfs=$?
+git diff --quiet HEAD master -- sql/
+skdb=$?
+
+cat .circleci/base.yml
+
+if (($skc == 0 && $skfs == 0 && $skdb == 0))
+then
+    circleci-agent step halt
+fi
+
+echo "workflows:"
+
+if (( $skc != 0 ))
+then
+    cat <<EOF
+  compiler:
+    jobs:
+      - compiler
+EOF
+fi
+
+if (( $skfs != 0 ))
+then
+    cat <<EOF
+  skfs:
+    jobs:
+      - skfs
+EOF
+fi
+
+if (( $skdb != 0 ))
+then
+    cat <<EOF
+  skdb:
+    jobs:
+      - skdb
+EOF
+fi
