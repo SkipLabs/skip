a457897c934dd235e4ebe64d06b36dcb3c96d642
Greg Sexton <gregsexton@gmail.com>
2023-08-31 15:15:08 +0100

    Use dockerignore instead of find|xargs rm
    Co-authored-by: Josh Berdine <josh@berdine.net>

--- sql/server/deploy/ref-server-build-push.sh
+++ sql/server/deploy/ref-server-build-push.sh
@@ -7,11 +7,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO="$SCRIPT_DIR/../../.."
 cd $REPO || exit 1
 
-find . -name state.db|xargs rm
+git clean -xdn | sed 's|Would remove |/|g' > .dockerignore
 
 echo "Building ref server image"
 # TODO: needs to buildx for x86 and arm
 docker build -t skdb-ref-server --progress=plain -f sql/server/reference/Dockerfile .
 
+git restore .dockerignore
 echo "Pushing image to docker"
 echo "TODO: NOT YET IMPLEMENTED"
