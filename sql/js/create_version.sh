88074ece3cfa3aed4eeeff4a169a19b19f38f50b
LOPES Daniel <daniel@skiplabs.io>
2023-09-15 14:38:49 +0200

    [skdb] Remove old js code

--- sql/js/create_version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-cd "$SCRIPT_DIR" || exit 1
-
-version=$(jq .version package.json)
-
-echo "export const npmVersion = $version" > src/version.ts
