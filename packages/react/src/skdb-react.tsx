9028bd54a31b4959ce17efadca7c61e5bc034516
Greg Sexton <gregsexton@gmail.com>
2023-09-26 14:11:46 +0100

    Create skdb-react package

---
+++ packages/react/src/skdb-react.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import { createContext, useContext } from 'react';
+import type { SKDB } from 'skdb'
+
+const SKDBContext = createContext<SKDB|undefined>(undefined);
+
+export function SKDBProvider({ children, skdb }: { children: React.ReactNode, skdb: SKDB }) {
+  return (
+    <SKDBContext.Provider value={skdb}>
+      {children}
+    </SKDBContext.Provider>
+  )
+}
+
+export function useSKDB(): SKDB {
+  const skdb = useContext(SKDBContext);
+
+  if (skdb === undefined) {
+    throw new Error("Missing SKDBProvider component.");
+  }
+
+  return skdb;
+}
+
+export function useQuery(
+  query: string,
+  params: Object = {},
+  defaultRows: Array<any> = [],
+): any {
+  const skdb = useSKDB();
+  const [state, setState] = useState(defaultRows);
+
+  const deps = Object.values(params);
+  deps.push(query);
+
+  useEffect(() => {
+    let removeQuery = false;
+    const closeable = { close: () => {} };
+    skdb.watch(query, params, (rows: Array<any>) => {
+      setState(rows);
+    }).then(handle => {
+      if (removeQuery) {
+        return handle.close();
+      }
+      closeable.close = handle.close
+    });
+    return () => {
+      removeQuery = true;
+      closeable.close();
+    };
+  }, deps);
+
+  return state;
+}
