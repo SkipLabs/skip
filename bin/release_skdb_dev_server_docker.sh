59ec96645183032a67c247f4bd472de98949f26b
Greg Sexton <gregsexton@gmail.com>
2023-10-02 16:19:40 +0100

    skdb dev server image script

--- bin/release_skdb_dev_server_docker.sh
+++ bin/release_skdb_dev_server_docker.sh
@@ -6,13 +6,22 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "$SCRIPT_DIR/../" || exit 1
 
-make test
+# make test
 
 # TODO: multiple platforms
-docker build -t skdb-dev-server --progress=plain -f sql/server/dev/Dockerfile .
+# docker build -t skdb-dev-server --progress=plain -f sql/server/dev/Dockerfile .
+
+docker buildx create --use
+
+docker buildx build \
+       --progress=plain \
+       --platform linux/amd64,linux/arm64 \
+       -t skiplabs/skdb-dev-server:latest \
+       --push \
+       .
 
 # TODO: old versions?
 
-docker tag skdb-dev-server:latest skiplabs/skdb-dev-server:latest
+# docker tag skdb-dev-server:latest skiplabs/skdb-dev-server:latest
 
-docker push skiplabs/skdb-dev-server:latest
+# docker push skiplabs/skdb-dev-server:latest
