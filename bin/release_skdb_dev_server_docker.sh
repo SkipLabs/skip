97eb42eca15120de3aaa83e07f5f8c166c32a0ce
Greg Sexton <gregsexton@gmail.com>
2023-10-04 11:04:44 +0100

    Remove stale commented out code

--- bin/release_skdb_dev_server_docker.sh
+++ bin/release_skdb_dev_server_docker.sh
@@ -6,11 +6,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "$SCRIPT_DIR/../" || exit 1
 
-# make test
-
-# TODO: multiple platforms
-# docker build -t skdb-dev-server --progress=plain -f sql/server/dev/Dockerfile .
-
 docker buildx create --use
 
 docker buildx build \
@@ -20,8 +15,4 @@ docker buildx build \
        --push \
        .
 
-# TODO: old versions?
-
-# docker tag skdb-dev-server:latest skiplabs/skdb-dev-server:latest
-
-# docker push skiplabs/skdb-dev-server:latest
+# TODO: keep versions and alias latest
