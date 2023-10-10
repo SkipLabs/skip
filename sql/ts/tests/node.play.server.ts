bbeab4cdbcf9e75dba7e718104c7a07db7e6e6af
LOPES Daniel <daniel@skiplabs.io>
2023-10-10 19:41:52 +0200

    [skdb] Split mux and server tests

--- sql/ts/tests/node.play.server.ts
+++ sql/ts/tests/node.play.server.ts
@@ -1,12 +1,6 @@
 import { test } from '@playwright/test';
-import { ms_tests } from './muxed_socket';
 import { setup, apitests } from './apitests';
 import { webcrypto as crypto } from 'crypto';
-import { WebSocket } from 'ws';
-import * as util from 'util';
-import * as mu from './muxed_utils.mjs';
-
-var encoder = new util.TextEncoder();
 
 
 function gatherCredential() {
@@ -22,19 +16,5 @@ function runServer(t, asWorker, suffix: string = "") {
   });
 }
 
-class Env {
-  crypto = () => crypto;
-  createSocket = (uri: string) => new WebSocket(uri);
-  encodeUTF8 = (v) => encoder.encode(v);
-}
-
-function runMS(t) {
-  test(t.name, async () => {
-    let res = await t.fun(new Env(), mu);
-    t.check(res);
-  });
-}
-
-ms_tests().forEach(t => runMS(t));
 apitests(false).forEach(t => runServer(t, false));
 apitests(true).forEach(t => runServer(t, true, "_node_worker"));
