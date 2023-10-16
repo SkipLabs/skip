e4ddc9f4a4265dc8c0835adb43f6fba86a6cbb6e
Josh Berdine <josh@skiplabs.io>
2023-10-16 16:55:53 +0100

    Enable parallel playwright tests in CI

--- sql/ts/tests/playwright.config.ts
+++ sql/ts/tests/playwright.config.ts
@@ -12,8 +12,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* CI workers have 4 CPUs */
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
