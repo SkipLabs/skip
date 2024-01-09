23928466289f5932947b0ec3927ba529a9a2f3c2
Greg Sexton <gregsexton@gmail.com>
2024-01-09 17:06:03 +0000

    Add a simple test to catch serious memory perf regression

--- sql/ts/tests/node.play.ts
+++ sql/ts/tests/node.play.ts
@@ -1,4 +1,4 @@
-import { test, type Page } from "@playwright/test";
+import { test, type Page, expect } from "@playwright/test";
 import { tests } from "./tests";
 import { createSkdb } from "skdb";
 
@@ -12,3 +12,30 @@ function run(t, asWorker: boolean) {
 
 tests(false).forEach((t) => run(t, false));
 tests(true).forEach((t) => run(t, true));
+
+// this is to detect memory regression in wasm, it's not a behaviour
+// test. running only in node as I saw timeout flakiness with firefox.
+run({
+  name: "Write-csv memory regression test",
+  fun: async (skdb: SKDB) => {
+    await skdb.exec(
+      "CREATE TABLE no_pk_inserts (id INTEGER, client INTEGER, value INTEGER, skdb_access STRING NOT NULL);",
+      {},
+    );
+
+    const N = 675000; // we should be able to process this many rows without running out of address space
+
+    const rows = ["^no_pk_inserts"];
+    for (let i = 0; i < N; i++) {
+      rows.push(`1\t${i},1,${i},read-write`);
+    }
+    rows.push(":10");
+
+    skdb.skdbSync.runLocal(["write-csv"], rows.join("\n") + '\n');
+
+    return await skdb.exec("select count(*) as n from no_pk_inserts");
+  },
+  check: (res) => {
+    expect(res).toEqual([{n: 675000}]);
+  },
+}, false);
