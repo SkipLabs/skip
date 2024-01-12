fac170883c7309ceabd1e4d463c76c3e5ba46acd
Benno Stein <benno.stein@gmail.com>
2024-01-12 11:41:15 +0000

    Bump timeout on api tests

--- sql/ts/tests/node.play.server.ts
+++ sql/ts/tests/node.play.server.ts
@@ -4,6 +4,7 @@ import { webcrypto as crypto } from "crypto";
 
 function runServer(t, asWorker, suffix: string = "") {
   test(t.name, async () => {
+    test.setTimeout(60000) // up from default 30 seconds
     let skdb = await setup(8110, crypto, asWorker, suffix);
     let res = await t.fun(skdb);
     t.check(res);
