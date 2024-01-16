8c3d295cf2b3c1a7a03abecbecbf8f980e3ddd58
Benno Stein <benno.stein@gmail.com>
2024-01-16 13:52:04 +0000

    Drop apitests timeout back to default (30s)

--- sql/ts/tests/node.play.server.ts
+++ sql/ts/tests/node.play.server.ts
@@ -4,7 +4,6 @@ import { webcrypto as crypto } from "crypto";
 
 function runServer(t, asWorker, suffix: string = "") {
   test(t.name, async () => {
-    test.setTimeout(60000) // up from default 30 seconds
     let skdb = await setup(8110, crypto, asWorker, suffix);
     let res = await t.fun(skdb);
     t.check(res);
