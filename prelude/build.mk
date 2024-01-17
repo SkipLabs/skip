d8ef4430c98af6adc199671c8243a611d09257a3
Josh Berdine <josh@berdine.net>
2024-01-17 09:55:26 +0000

    Compile 64 runtime to bitcode like the 32 bit one

--- prelude/build.mk
+++ prelude/build.mk
@@ -1,50 +1,10 @@
 SCRIPT_DIR:=$(shell dirname $(shell realpath $(firstword $(MAKEFILE_LIST))))
+
 COMP_DIR:=$(shell realpath $(SCRIPT_DIR)/../compiler)
 
 LIB_DIR?=/usr/local/lib
 
-ifdef PROFILE
-PRFDEF=$(shell echo $(PROFILE) | tr  '[:lower:]' '[:upper:]')
-DEFINITIONS=-D$(PRFDEF)
-ifeq ($(PRFDEF),RELEASE)
-OLEVEL=-O3
-else
-ifeq ($(PRFDEF),DEBUG)
-OLEVEL=-O0
-else
-OLEVEL=-O2
-endif # ifeq ($(PRFDEF),DEBUG)
-endif # ifeq ($(PRFDEF),RELEASE)
-else
-DEFINITIONS=
-OLEVEL=-O2 -g
-endif # ifdef PROFILE
-
 LBT_EXISTS=$(shell [ -e $(LIB_DIR)/libbacktrace.a ] && echo 1 || echo 0 )
-CC64FLAGS=$(OLEVEL) -DSKIP64
-
-# NB: this MUST be kept in sync with CFILES in compiler/runtime/Makefile
-# and CRELFILES in prelude/build_wasm32.mk
-CRELFILES=\
-	runtime/copy.c \
-	runtime/free.c \
-	runtime/hash.c \
-	runtime/hashtable.c \
-	runtime/intern.c \
-	runtime/memory.c \
-	runtime/obstack.c \
-	runtime/runtime.c \
-	runtime/stdlib.c \
-	runtime/stack.c \
-	runtime/string.c \
-	runtime/posix.c \
-	runtime/native_eq.c \
-	runtime/splitmix64.c \
-	runtime/xoroshiro128plus.c
-
-NATIVE_RELFILES=\
-	runtime/palloc.c\
-	runtime/consts.c
 
 ifdef OUT_DIR
 BUILD_DIR=$(shell realpath $(OUT_DIR))/
@@ -52,30 +12,12 @@ else
 BUILD_DIR=$(ROOT_DIR)/build/
 endif
 
-CFILES=$(addprefix $(COMP_DIR)/,$(CRELFILES))
-NATIVE_FILES=$(addprefix $(COMP_DIR)/,$(NATIVE_RELFILES))
-
-OFILES=$(addprefix $(BUILD_DIR),$(CRELFILES:.c=.o))
-ONATIVE_FILES=$(BUILD_DIR)magic.o $(addprefix $(BUILD_DIR),$(NATIVE_RELFILES:.c=.o))
-
 .PHONY: default
-default: $(BUILD_DIR)libbacktrace.a $(BUILD_DIR)libskip_runtime64.a
-	@echo "skargo:library=$(BUILD_DIR)libskip_runtime64.a"
+default: $(BUILD_DIR)libbacktrace.a
+	@echo "skargo:library=$(COMP_DIR)/runtime/libskip_runtime64.bc"
 	@echo "skargo:library=$(BUILD_DIR)libbacktrace.a"
 	@echo "skargo:preamble=$(COMP_DIR)/preamble/preamble64.ll"
 	@echo "skargo:link=-lpthread"
-
-$(BUILD_DIR)magic.c:
-	@date | cksum | awk '{print "unsigned long version = " $$1 ";"}' > $(BUILD_DIR)magic.c
-	@echo "int SKIP_get_version() { return (int)version; }" >> $(BUILD_DIR)magic.c
-
-$(BUILD_DIR)magic.o: $(BUILD_DIR)magic.c
-	@[ -d $(dir $@) ] || mkdir -p $(dir $@)
-	@clang $(CC64FLAGS) -o $@ -c $<
-
-$(BUILD_DIR)libskip_runtime64.a: $(OFILES) $(BUILD_DIR)runtime/runtime64_specific.o $(ONATIVE_FILES)
-	@ar rcs $@ $^
-
 
 ifeq ($(LBT_EXISTS), 1)
 $(BUILD_DIR)libbacktrace.a:
@@ -87,11 +29,3 @@ $(BUILD_DIR)libbacktrace.a:
 	$(MAKE) -C $(COMP_DIR)/runtime/libbacktrace
 	cp $(COMP_DIR)/runtime/libbacktrace/.libs/libbacktrace.a $(BUILD_DIR)libbacktrace.a
 endif
-
-$(BUILD_DIR)runtime/runtime64_specific.o: $(COMP_DIR)/runtime/runtime64_specific.cpp
-	@[ -d $(dir $@) ] || mkdir -p $(dir $@)
-	@clang++ $(OLEVEL) -g -o $@ -c -I$(COMP_DIR)/runtime/libbacktrace/ $<
-
-$(BUILD_DIR)%.o: $(COMP_DIR)/%.c
-	@[ -d $(dir $@) ] || mkdir -p $(dir $@)
-	@clang $(CC64FLAGS) -g -o $@ -c $<
