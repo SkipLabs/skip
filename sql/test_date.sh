57b5eb0025b09e551bf53027dabb6b07db931ef7
Benno Stein <benno.stein@gmail.com>
2024-01-17 15:10:24 +0000

    Parallelize many date tests; add support for more pregenerated input

--- sql/test_date.sh
+++ sql/test_date.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 if [ -z "$SKARGO_PROFILE" ]; then
@@ -60,6 +59,34 @@ if [ ! -f "test/dates/valid_date_list2.txt" ]; then
   done < "test/dates/valid_date_list.txt" > "test/dates/valid_date_list2.txt"
 fi
 
+if [ ! -f "test/dates/days.sql" ]; then
+  while IFS="" read -r d || [ -n "$d" ]
+  do
+    echo "select strftime('%Y-%m-%d %H:%M:%S', '$d');"
+  done < "test/dates/valid_date_list3.txt" > "test/dates/days.sql"
+fi
+
+if [ ! -f "test/dates/weeknumbers.sql" ]; then
+  while IFS="" read -r d || [ -n "$d" ]
+  do
+    echo "select strftime('%Y-%m-%d: %W', '$d');"
+  done < "test/dates/valid_date_list.txt" > "test/dates/weeknumbers.sql"
+fi
+
+if [ ! -f "test/dates/plus_one_day.sql" ]; then
+  while IFS="" read -r d || [ -n "$d" ]
+  do
+    echo "select strftime('%Y-%m-%d %H:%M:%S', '$d', '+1 day');"
+  done < "test/dates/valid_date_list.txt" > "test/dates/plus_one_day.sql"
+fi
+
+if [ ! -f "test/dates/minus_one_day.sql" ]; then
+  while IFS="" read -r d || [ -n "$d" ]
+  do
+    echo "select strftime('%Y-%m-%d %H:%M:%S', '$d', '-1 day');"
+  done < "test/dates/valid_date_list.txt" > "test/dates/minus_one_day.sql"
+fi
+
 run_test () {
   cat $1 | $SKDB --always-allow-joins | sort > "$1.out";
   cat $1 | sqlite3 | sort > "$1.res"
@@ -67,7 +94,6 @@ run_test () {
   if [ "$diff" == "" ]; then
       rm -f "$1.out"
       rm -f "$1.res"
-      rm -f "$1"
       pass "$2"
   else
       echo $diff > "$1.diff"
@@ -136,41 +162,13 @@ run_date_test () {
   fi
 }
 
-run_date_test test/dates/formats_1.sql "formats 1" "2023-09-18 13:26:54"
-run_date_test test/dates/formats_2.sql "formats 2" "2021-01-27 08:52:03"
-run_date_test test/dates/formats_3.sql "formats 3" "1985-01-01 00:00:00"
-run_date_test test/dates/formats_4.sql "formats 4" "1963-12-31 00:00:00"
-
-
 ##############################################################################
 # testing weeknumbers #
 ##############################################################################
 
-while IFS="" read -r d || [ -n "$d" ]
-do
-  echo "select strftime('%Y-%m-%d: %W', '$d');"
-done < "test/dates/valid_date_list.txt" > "test/dates/weeknumbers.sql"
-run_test "test/dates/weeknumbers.sql" "weeknumbers"
-
-
 ##############################################################################
 # testing +1 day #
 ##############################################################################
-
-
-while IFS="" read -r d || [ -n "$d" ]
-do
-  echo "select strftime('%Y-%m-%d %H:%M:%S', '$d', '+1 day');"
-done < "test/dates/valid_date_list.txt" > "test/dates/plus_one_day.sql"
-
-run_test test/dates/plus_one_day.sql "+1 day (at hour 00:00)"
-
-while IFS="" read -r d || [ -n "$d" ]
-do
-  echo "select strftime('%Y-%m-%d %H:%M:%S', '$d', '-1 day');"
-done < "test/dates/valid_date_list.txt" > "test/dates/minus_one_day.sql"
-
-run_test test/dates/minus_one_day.sql "-1 day (at hour 00:00)"
 
 ##############################################################################
 # testing modifiers with hours #
@@ -180,63 +178,33 @@ run_hour_test () {
     for i in {"23:00","23:30","00:00","00:30","01:00"}; do
         tmp=${1/+/plus}
         suffix=${tmp// /_}_${i//:/_}
-        while IFS="" read -r d || [ -n "$d" ]
-        do
-          echo "select strftime('%Y-%m-%d %H:%M:%S', '$d $i', '$1');" 
-        done < "test/dates/valid_date_list2.txt" > "test/dates/hour_$suffix.sql"
-
+	if [ ! -f "test/dates/hour_$suffix.sql" ]; then
+          while IFS="" read -r d || [ -n "$d" ]
+          do
+            echo "select strftime('%Y-%m-%d %H:%M:%S', '$d $i', '$1');"
+          done < "test/dates/valid_date_list2.txt" > "test/dates/hour_$suffix.sql"
+	fi
         run_test test/dates/hour_$suffix.sql "$1 (at hour $i)"
     done
 }
-
-
-for i in {0..32}; do
-    run_hour_test "+$i day"
-done
 
 run_hour_test2 () {
     for i in {"23:00","23:30","00:00","00:30","01:00"}; do
         tmp=${1/-/minus}
         suffix=${tmp// /_}_${i//:/_}
-        while IFS="" read -r d || [ -n "$d" ]
-        do
-          echo "select strftime('%Y-%m-%d %H:%M:%S', '$d $i', '$1');"
-        done < "test/dates/valid_date_list2.txt" > "test/dates/hour_$suffix.sql"
-
+	if [ ! -f "test/dates/hour_$suffix.sql" ]; then
+          while IFS="" read -r d || [ -n "$d" ]
+          do
+            echo "select strftime('%Y-%m-%d %H:%M:%S', '$d $i', '$1');"
+          done < "test/dates/valid_date_list2.txt" > "test/dates/hour_$suffix.sql"
+	fi
         run_test test/dates/hour_$suffix.sql "$1 (at hour $i)"
     done
 }
 
-for i in {0..32}; do
-    run_hour_test2 "-$i day"
-done
-
-run_hour_test2 "-365 day"
-run_hour_test2 "-366 day"
-
-for i in {0..6}; do
-    run_hour_test "weekday $i"
-done
-
-for i in {0..13}; do
-    run_hour_test "+$i month"
-done
-
-for i in {0..13}; do
-    run_hour_test2 "-$i month"
-done
-
-
 ##############################################################################
 # testing days #
 ##############################################################################
-
-while IFS="" read -r d || [ -n "$d" ]
-do
-  echo "select strftime('%Y-%m-%d %H:%M:%S', '$d');"
-done < "test/dates/valid_date_list3.txt" > "test/dates/days.sql"
-
-run_test test/dates/days.sql "days"
 
 ##############################################################################
 # modify time #
@@ -244,16 +212,13 @@ run_test test/dates/days.sql "days"
 
 run_modify_time () {
   date=$(date +"%Y-%m-%d %H:%M:%S")
-  for i in {1..20500}; do
-    echo "select strftime('%Y-%m-%d %H:%M:%S', '$date',  '$2$i $3');" 
-  done  > "test/dates/$1.sql"
+  if [ ! -f "test/dates/$1.sql" ]; then
+    for i in {1..20500}; do
+      echo "select strftime('%Y-%m-%d %H:%M:%S', '$date',  '$2$i $3');"
+    done  > "test/dates/$1.sql"
+  fi
   run_test "test/dates/$1.sql" $1
 }
-
-run_modify_time "add_hours" "+" "hour"
-run_modify_time "minus_hours" "-" "hour"
-run_modify_time "add_minutes" "+" "minute"
-run_modify_time "minus_minute" "-" "minute"
 
 ##############################################################################
 # testing epoch #
@@ -277,8 +242,6 @@ run_epoch_utc_test () {
   fi
 }
 
-run_epoch_utc_test "test/dates/epoch_utc.sql" "epoch_utc"
-
 run_epoch_locale_test () {
   while IFS="" read -r d || [ -n "$d" ]
   do
@@ -297,9 +260,34 @@ run_epoch_locale_test () {
   fi
 }
 
+# Make functions defined in this file visible to parallel's sub-shells
+export -f pass fail run_date run_u_date run_hour_test run_hour_test2 run_date_test run_test run_modify_time
+
+run_hour_test2 "-365 day"
+run_hour_test2 "-366 day"
+
+seq 0 6 | parallel 'run_hour_test "weekday {}"'
+seq 0 13 | parallel 'run_hour_test "+{} month" ; run_hour_test2 "-{} month"'
+seq 0 32 | parallel 'run_hour_test "+{} day" ; run_hour_test2 "-{} day"'
+
+parallel -n3 run_date_test ::: \
+	 test/dates/formats_1.sql "formats 1" "2023-09-18 13:26:54" \
+	 test/dates/formats_2.sql "formats 2" "2021-01-27 08:52:03" \
+	 test/dates/formats_3.sql "formats 3" "1985-01-01 00:00:00" \
+	 test/dates/formats_4.sql "formats 4" "1963-12-31 00:00:00"
+
+
+parallel -n2 run_test ::: \
+	 test/dates/weeknumbers.sql "weeknumbers" \
+	 test/dates/plus_one_day.sql "+1 day (at hour 00:00)" \
+	 test/dates/minus_one_day.sql "-1 day (at hour 00:00)" \
+	 test/dates/days.sql "days"
+
+parallel -n3 run_modify_time ::: \
+	 "add_hours" "+" "hour" \
+	 "minus_hours" "-" "hour" \
+	 "add_minutes" "+" "minute" \
+	 "minus_minute" "-" "minute"
+
+run_epoch_utc_test "test/dates/epoch_utc.sql" "epoch_utc"
 run_epoch_locale_test "test/dates/epoch_locale.sql" "epoch_locale"
-
-
-
-
-
