9b129bf4cf79abf91f48eff9adfb8cbb709e3623
Benno Stein <benno.stein@gmail.com>
2024-01-30 10:56:51 -0500

    Add script to release amd64/arm64 skiplabs/skdb docker images

--- bin/release_docker_skdb.sh
+++ bin/release_docker_skdb.sh
@@ -11,37 +11,19 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd "$SCRIPT_DIR/../" || exit 1
 
-read -r -p "Release 'latest' version? [y/N] " latest
-if [[ "$latest" =~ ^([yY]|[yY][eE][sS])$ ]]; then
-    push_latest="-t skiplabs/skdb-dev-server:latest"
-fi
-
-read -r -p "Release 'quickstart' version? [y/N] " quickstart
-if [[ "$quickstart" =~ ^([yY]|[yY][eE][sS])$ ]]; then
-    push_quickstart="-t skiplabs/skdb-dev-server:quickstart"
-fi
-
-if [[ -z "$push_latest" && -z "$push_quickstart" ]]; then
-    echo Nothing to do, exiting.
-    exit 0
-fi
-
 git clean -xd --dry-run | sed 's|Would remove |/|g' >> .dockerignore
 echo ".git" >> .dockerignore
 
-builder=$(docker buildx create --platform linux/amd64,linux/arm64 --use)
+builder=$(docker buildx create --use)
 
 docker buildx build \
        --no-cache \
        --progress=plain \
        --platform linux/amd64,linux/arm64 \
-       $push_latest \
-       $push_quickstart \
-       -f sql/server/dev/Dockerfile \
+       -t skiplabs/skdb:latest \
+       -f sql/Dockerfile \
        --push \
        .
-
-# TODO: keep versions and alias latest
 
 docker buildx stop "$builder"
 docker buildx rm "$builder"
