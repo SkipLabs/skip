211eacc3c4a34c2c5a9c5a6bbb470970843b705a
Greg Sexton <gregsexton@gmail.com>
2024-02-20 17:58:40 +0000

    Run prettier

--- sql/ts/src/skdb_wasm_locator.ts
+++ sql/ts/src/skdb_wasm_locator.ts
@@ -3,7 +3,7 @@ import type { Environment } from "#std/sk_types.js";
 
 export async function getWasmUrl(env: Environment): Promise<string> {
   if (isNode()) {
-    return relativeto(resolve("./skdb.wasm"), env.rootPath())
+    return relativeto(resolve("./skdb.wasm"), env.rootPath());
   } else {
     //@ts-ignore
     const url = await import("./skdb.wasm?url");
