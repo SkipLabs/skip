7a5e094b84651dd75d6b5eeb0644ae9a890b29cf
Greg Sexton <gregsexton@gmail.com>
2024-02-21 12:34:58 +0000

    Fix: allow skdb package to be imported without a bundler
    This fixes all the tests and opens up another important way of using
    the package.
    
    The actual fix is a little sneaky/hacky - as with all things to do
    with JS build/deploy :(. We use import.meta which should be available
    
    in all contexts. vite and webpack set specific properties on this
    object that we can look for. So this solution likely doesn't work for
    other bundlers.

--- sql/ts/src/skdb_wasm_locator.ts
+++ sql/ts/src/skdb_wasm_locator.ts
@@ -2,11 +2,12 @@ import { isNode, relativeto, resolve } from "#std/sk_types.js";
 import type { Environment } from "#std/sk_types.js";
 
 export async function getWasmUrl(env: Environment): Promise<string> {
-  if (isNode()) {
-    return relativeto(resolve("./skdb.wasm"), env.rootPath());
-  } else {
+  //@ts-ignore
+  if (import.meta.env || import.meta.webpack) {
     //@ts-ignore
     const url = await import("./skdb.wasm?url");
     return url.default;
   }
+
+  return relativeto(resolve("./skdb.wasm"), env.rootPath());
 }
