f00cdca3ea65083874ce2752717f68a679d7c220
Benno Stein <benno.stein@gmail.com>
2024-03-19 18:43:21 +0000

    Drop useless spotless config from root build.gradle.kts

--- sql/server/build.gradle.kts
+++ sql/server/build.gradle.kts
@@ -1,8 +1,3 @@
 plugins {
   id("org.jetbrains.kotlin.jvm") version "1.9.0"
-  id("com.diffplug.spotless") version "6.12.1"
 }
-
-repositories { mavenCentral() }
-
-spotless { kotlin { ktfmt("0.47") } }
