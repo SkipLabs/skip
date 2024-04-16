dfb51d47798d50296f1943385bc4ba63f2c744af
Greg Sexton <gregsexton@gmail.com>
2024-04-16 15:56:31 +0100

    Ensure table aliases work

--- sql/test/unit/checks/fail_select_from_table.sql
+++ sql/test/unit/checks/fail_select_from_table.sql
@@ -1,5 +1,5 @@
 create table if not exists test(x integer);
 delete from test;
 insert into test values (0), (1), (2);
-select check(x < 2) from test;
+select check(t.x < 2) from test t;
 -- should fail
