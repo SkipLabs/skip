ae4b5e303853e4f0739bbe90927b2c41e568bb2d
Benno Stein <benno.stein@gmail.com>
2024-05-17 17:31:05 +0100

    Rename skdb_original column indicating extracted column

--- sql/test/unit/test_alter_table_extract_col.sql
+++ sql/test/unit/test_alter_table_extract_col.sql
@@ -7,12 +7,12 @@ INSERT INTO t2 (a, b) VALUES (1, 2), (3, 4);
 CREATE REACTIVE VIEW vv AS SELECT a, y FROM t1 INNER JOIN t2 on (a * 10) = x;
 
 BEGIN TRANSACTION;
-      CREATE TABLE t1_x AS SELECT id, x, true AS __skdb_is_original FROM t1;
+      CREATE TABLE t1_x AS SELECT id, x, true AS skdb_original FROM t1;
       ALTER TABLE t1 DROP COLUMN x;
 COMMIT;
 
 
 INSERT INTO t1 VALUES (10, 11);
 
-SELECT t1_x.x, t1_x.__skdb_is_original, t1.y, t1.id FROM t1 INNER JOIN t1_x on t1.id = t1_x.id;
+SELECT t1_x.x, t1_x.skdb_original, t1.y, t1.id FROM t1 INNER JOIN t1_x on t1.id = t1_x.id;
 SELECT * FROM vv;
