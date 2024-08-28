59a11a7c3d9595175612a9d33e0717ae84b5ea89
Mehdi Bouaziz <mehdi@skiplabs.io>
2024-08-28 12:34:02 +0200

    Fix lint errors in skstore_impl.ts

--- skstore/ts/src/eslint.config.js
+++ skstore/ts/src/eslint.config.js
@@ -6,4 +6,35 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          args: "none",
+          caughtErrors: "all",
+          ignoreRestSiblings: true,
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          reportUsedIgnorePattern: true,
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "none",
+          caughtErrors: "all",
+          ignoreRestSiblings: true,
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          reportUsedIgnorePattern: true,
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 );
