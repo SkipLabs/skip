588cee71daa80a0ea26f12fd0d32c47ee150c4f7
LOPES Daniel <daniel@skiplabs.io>
2024-08-28 10:16:01 +0200

    [skstore] Add NonEmptyIteratorImpl utils methods

--- skipruntime-ts/ts/src/skipruntime_api.ts
+++ skipruntime-ts/ts/src/skipruntime_api.ts
@@ -239,6 +239,23 @@ export interface NonEmptyIterator<T> extends Iterable<T> {
    * Returns an array containing the remaining values of the iterator
    */
   toArray: () => T[];
+
+  /**
+   * Performs the specified action for each element in the iterator.
+   * @param callbackfn  A function that accepts up to thow arguments. forEach calls the callbackfn function one time for each element.
+   * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+   */
+  forEach(callbackfn: (value: T, index: number) => void): void;
+
+  /**
+   * Calls a defined callback function on each element of an array, and returns an array that contains the results.
+   * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+   * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+   */
+  map<U>(
+    callbackfn: (value: T, index: number) => U,
+    thisArg?: any,
+  ): Iterable<U>;
 }
 
 /**
