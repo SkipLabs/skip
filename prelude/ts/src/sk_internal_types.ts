249391c8be238c6075a2a5bbafb9ec47abc325a1
Mehdi Bouaziz <mehdi@skiplabs.io>
2024-08-30 18:13:57 +0200

    Strengthen ptr

--- prelude/ts/src/sk_internal_types.ts
+++ prelude/ts/src/sk_internal_types.ts
@@ -2,7 +2,7 @@ declare const opaque: unique symbol;
 export type Opaque<Value, Tag> = Value & { [opaque]: Tag };
 
 declare const t: unique symbol;
-export type T<V = any> = { [t]: V };
+export type T<V = unknown> = { [t]: V };
 
 export type Any = T;
 
