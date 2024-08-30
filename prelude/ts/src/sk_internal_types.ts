1d7c76285410755c85cde51d68260b41567ee558
Mehdi Bouaziz <mehdi@skiplabs.io>
2024-08-30 16:02:33 +0200

    Type call and etry

--- prelude/ts/src/sk_internal_types.ts
+++ prelude/ts/src/sk_internal_types.ts
@@ -6,6 +6,9 @@ export type T<V = any> = { [t]: V };
 
 export type Any = T;
 
+declare const void_: unique symbol;
+export type Void = T<typeof void_>;
+
 declare const byte: unique symbol;
 export type Byte = T<typeof byte>;
 
@@ -29,4 +32,9 @@ declare const p: unique symbol;
 declare const array: unique symbol;
 export type Array<Ty extends T> = T<typeof array> & {
   [p]: (_: Ty) => Ty;
+};
+
+declare const function_: unique symbol;
+export type Function<Ty1 extends T, Ty2 extends T> = T<typeof function_> & {
+  [p]: (_: Ty1) => Ty2;
 };
