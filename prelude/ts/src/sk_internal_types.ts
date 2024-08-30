75600718e7628a35cc992e1c6838d8be7ae681a0
Mehdi Bouaziz <mehdi@skiplabs.io>
2024-08-30 12:32:12 +0200

    Partially type pointers in prelude/ts
    No issues found apart from a confusion between exceptions and strings in a few places.

--- prelude/ts/src/sk_internal_types.ts
+++ prelude/ts/src/sk_internal_types.ts
@@ -5,3 +5,28 @@ declare const t: unique symbol;
 export type T<V = any> = { [t]: V };
 
 export type Any = T;
+
+declare const byte: unique symbol;
+export type Byte = T<typeof byte>;
+
+declare const uint32: unique symbol;
+export type UInt32 = T<typeof uint32>;
+
+declare const float: unique symbol;
+export type Float = T<typeof float>;
+
+declare const str: unique symbol;
+export type String = T<typeof str>;
+
+declare const exception: unique symbol;
+export type Exception = T<typeof exception>;
+
+declare const obstack: unique symbol;
+export type Obstack = T<typeof obstack>;
+
+declare const p: unique symbol;
+
+declare const array: unique symbol;
+export type Array<Ty extends T> = T<typeof array> & {
+  [p]: (_: Ty) => Ty;
+};
