b1d571c28b1daebb5fd7d3a1ca97cbe36e3d653e
Benno Stein <benno.stein@gmail.com>
2024-08-30 18:13:29 +0100

    Chase build artifacts and packages to name consistently as "skip_runtime"

--- skruntime-ts/ts/tests/tests.ts
+++ skruntime-ts/ts/tests/tests.ts
@@ -16,7 +16,7 @@ import type {
   Loadable,
   NonEmptyIterator,
   AsyncLazyCollection,
-} from "skstore";
+} from "skip_runtime";
 import {
   Sum,
   ValueMapper,
@@ -24,7 +24,7 @@ import {
   cinteger as integer,
   schema,
   ctext as text,
-} from "skstore";
+} from "skip_runtime";
 
 function check(name: String, got: TJSON, expected: TJSON): void {
   expect([name, got]).toEqual([name, expected]);
