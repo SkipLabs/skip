5b3b305756e9d762297105afdd68c2ce786cdccd
Josh Berdine <josh@skiplabs.io>
2024-09-04 15:31:07 +0100

    Add initial skeleton based on docusaurus template
    Created by:
    ```
    - cd /path/to/repo/root
    - npx create-docusaurus@latest www classic --typescript
    ```
    and then adjusting the template content.

---
+++ www/docusaurus.config.ts
@@ -0,0 +1,141 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Skip",
+  tagline: "A generic framework for reactive programming",
+  favicon: "img/favicon.svg",
+
+  // Set the production url of your site here
+  url: "https://skiplabsdocscms-00-pbrstreetgang-00s-projects.vercel.app/",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "skiplabs", // Usually your GitHub org/user name.
+  projectName: "skip", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Remove this to remove the "edit this page" links.
+          // editUrl: "",
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   feedOptions: {
+        //     type: ["rss", "atom"],
+        //     xslt: true,
+        //   },
+        //   // Remove this to remove the "edit this page" links.
+        //   // editUrl: "",
+        //   // Useful options to enforce blogging best practices
+        //   onInlineTags: "warn",
+        //   onInlineAuthors: "warn",
+        //   onUntruncatedBlogPosts: "warn",
+        // },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/skiplabs.svg",
+    navbar: {
+      title: "Skip",
+      logo: {
+        alt: "Skip Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "Docs",
+        },
+        // { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/SkipLabs/skdb",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Overview",
+              to: "/docs/overview",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/SkipLabs/skdb",
+            },
+            // {
+            //   label: "Stack Overflow",
+            //   href: "https://stackoverflow.com/questions/tagged/skiplabs",
+            // },
+            {
+              label: "Discord",
+              href: "https://discord.gg/4dMEBA46mE",
+            },
+            // {
+            //   label: "Twitter",
+            //   href: "https://twitter.com/skiplabs",
+            // },
+          ],
+        },
+        // {
+        //   title: "More",
+        //   items: [
+        //     {
+        //       label: "Blog",
+        //       to: "/blog",
+        //     },
+        //     {
+        //       label: "GitHub",
+        //       href: "https://github.com/SkipLabs/skdb",
+        //     },
+        //   ],
+        // },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} SkipLabs, Inc.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
