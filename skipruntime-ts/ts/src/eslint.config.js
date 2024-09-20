267839ece45428cf5d8976c96d1cdf2673def684
Josh Berdine <josh@skiplabs.io>
2024-09-20 21:20:25 +0100

    Type-check with noUncheckedIndexedAccess when linting
    Otherwise type-based linting is nonsensical.

--- skipruntime-ts/ts/src/eslint.config.js
+++ skipruntime-ts/ts/src/eslint.config.js
@@ -16,7 +16,7 @@ export default tseslint.config(
     },
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        project: "./tsconfig-eslint.json",
         tsconfigRootDir: import.meta.dirname,
       },
     },
