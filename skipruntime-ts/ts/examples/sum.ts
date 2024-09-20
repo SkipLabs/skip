ba116cb85e4971ec381827612365f7da885eb1bd
Josh Berdine <josh@skiplabs.io>
2024-09-20 22:45:19 +0100

    Tighten types in sum example

--- skipruntime-ts/ts/examples/sum.ts
+++ skipruntime-ts/ts/examples/sum.ts
@@ -6,24 +6,27 @@ import type {
   NonEmptyIterator,
   SimpleSkipService,
   SimpleServiceOutput,
-  JSONObject,
   Writer,
 } from "skip-runtime";
 
 import { runWithServer } from "skip-runtime";
 
-class Request implements Mapper<string, TJSON, string, TJSON> {
-  constructor(private source: EagerCollection<string, TJSON>) {}
+type Command =
+  | { command: "add"; payload: string }
+  | { command: "set"; payload: { name: string; key: string; value: number }[] }
+  | { command: "delete"; payload: { name: string; keys: string[] }[] };
+
+class Request implements Mapper<string, Command, string, number> {
+  constructor(private source: EagerCollection<string, number>) {}
 
   mapElement(
     key: string,
-    it: NonEmptyIterator<TJSON>,
-  ): Iterable<[string, TJSON]> {
-    const v = it.first() as JSONObject;
+    it: NonEmptyIterator<Command>,
+  ): Iterable<[string, number]> {
+    const v = it.first();
     let computed = 0;
     if (v.command == "add") {
-      const value = this.source.maybeGetOne(v.payload as string) as number;
-      computed = value ? value : 0;
+      computed = this.source.maybeGetOne(v.payload) ?? 0;
     }
     return Array([key, computed]);
   }
@@ -46,14 +49,6 @@ class Add implements Mapper<string, number, string, number> {
   }
 }
 
-type Command = {
-  command: string;
-  payload: TJSON;
-};
-
-type Set = { name: string; key: string; value: number };
-type Delete = { name: string; keys: string[] };
-
 class Service implements SimpleSkipService {
   name: string = "sum";
   inputTables = ["input1", "input2"];
@@ -64,26 +59,26 @@ class Service implements SimpleSkipService {
 
   reactiveCompute(
     _store: SKStore,
-    requests: EagerCollection<string, TJSON>,
+    requests: EagerCollection<string, Command>,
     inputCollections: Record<string, EagerCollection<string, number>>,
   ): SimpleServiceOutput {
     const addResult = inputCollections.input1.map(Add, inputCollections.input2);
     const output = requests.map(Request, addResult);
     return {
       output,
-      update: (event: TJSON, writers: Record<string, Writer<TJSON>>) => {
+      update: (event: TJSON, writers: Record<string, Writer<number>>) => {
         const cmd = event as Command;
         if (cmd.command == "set") {
-          const payload = cmd.payload as Set[];
-          for (const e of payload) {
-            const writer = writers[e.name];
-            writer.set(e.key, e.value);
+          const payload = cmd.payload;
+          for (const entry of payload) {
+            const writer = writers[entry.name];
+            writer.set(entry.key, entry.value);
           }
         } else if (cmd.command == "delete") {
-          const payload = cmd.payload as Delete[];
-          for (const e of payload) {
-            const writer = writers[e.name];
-            writer.delete(e.keys);
+          const payload = cmd.payload;
+          for (const entry of payload) {
+            const writer = writers[entry.name];
+            writer.delete(entry.keys);
           }
         }
       },
