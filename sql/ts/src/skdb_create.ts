c9ec8209cc11a11fabddf14653074b8801196940
Mehdi Bouaziz <mehdi@skiplabs.io>
2024-09-23 16:43:02 +0200

    Merge run and runUrl

--- sql/ts/src/skdb_create.ts
+++ sql/ts/src/skdb_create.ts
@@ -1,4 +1,4 @@
-import { runUrl, type ModuleInit } from "std";
+import { run, type ModuleInit } from "std";
 import type { SKDBShared } from "./skdb_types.js";
 import { getWasmUrl } from "./skdb_wasm_locator.js";
 
@@ -8,7 +8,7 @@ export async function createOnThisThread(
   dbName?: string,
   getWasmSource?: () => Promise<Uint8Array>,
 ) {
-  const data = await runUrl(
+  const data = await run(
     getWasmUrl,
     // @ts-ignore
     modules,
