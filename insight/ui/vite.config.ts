295f2d49cf018020ff3c008f9231494caa08f543
Greg Sexton <gregsexton@gmail.com>
2024-09-25 12:08:50 +0100

    Build insight UI
    This renders a debug trace given a static JSON file that is generated
    by SKStore.

---
+++ insight/ui/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  worker: {
+    format: 'es'
+  },
+  optimizeDeps: {
+    exclude: ['skdb'],
+  }
+})
