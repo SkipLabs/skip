a2c23cf85e331b85c2b87da643fdc9a2d84b25e8
Mehdi Bouaziz <mehdi@skiplabs.io>
2024-10-01 10:31:39 +0200

    Simplify import-export

--- skipruntime-ts/core/src/skip-runtime.ts
+++ skipruntime-ts/core/src/skip-runtime.ts
@@ -1,31 +1,9 @@
-import type { SkipService, Resource } from "./skipruntime_service.js";
-import type {
-  SKStore,
-  TJSON,
-  JSONObject,
-  Accumulator,
-  SkipRuntime,
-  Entry,
-  ReactiveResponse,
-} from "./skipruntime_api.js";
-
+export type { SkipService, Resource } from "./skipruntime_service.js";
 export { UnknownCollectionError } from "./skipruntime_errors.js";
 export type { Opaque } from "./internals/skipruntime_module.js";
 export { TimedQueue } from "./internals/skipruntime_module.js";
 export { runService as initService } from "./skipruntime_runner.js";
 export { createSKStore } from "./skipruntime_init.js";
-
-export type {
-  SKStore,
-  TJSON,
-  JSONObject,
-  Accumulator,
-  SkipService,
-  Resource,
-  SkipRuntime,
-  Entry,
-  ReactiveResponse,
-};
 
 export type {
   AValue,
@@ -41,6 +19,13 @@ export type {
   AsyncLazyCollection,
   EntryPoint,
   ExternalCall,
+  SKStore,
+  TJSON,
+  JSONObject,
+  Accumulator,
+  SkipRuntime,
+  Entry,
+  ReactiveResponse,
 } from "./skipruntime_api.js";
 
 export { ValueMapper } from "./skipruntime_api.js";
