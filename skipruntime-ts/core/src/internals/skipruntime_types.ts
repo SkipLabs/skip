4751190d3e819738f51414a2ac97b61cb145c20f
Mehdi Bouaziz <mehdi@skiplabs.io>
2024-10-02 12:03:14 +0200

    Remove unused Handles.name

--- skipruntime-ts/core/src/internals/skipruntime_types.ts
+++ skipruntime-ts/core/src/internals/skipruntime_types.ts
@@ -1,4 +1,4 @@
-import type { Opt, Shared, ptr, int, float, Metadata } from "std";
+import type { Opt, Shared, ptr, int, float } from "std";
 import type * as Internal from "./skipruntime_internal_types.js";
 import type {
   Accumulator,
@@ -168,7 +168,6 @@ export interface Handles {
   get<T>(id: Handle<T>): T;
   apply<R, P extends any[]>(id: Handle<(..._: P) => R>, parameters: P): R;
   deleteHandle<T>(id: Handle<T>): T;
-  name(metadata: Metadata): string;
 }
 
 export interface FromWasm {
