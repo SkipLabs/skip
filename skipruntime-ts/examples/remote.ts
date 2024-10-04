986ad4bd7d3383ff04baa4c599724bf1f2c12db3
LOPES Daniel <daniel@skiplabs.io>
2024-10-04 22:00:39 +0200

    Add remote basic example

---
+++ skipruntime-ts/examples/remote.ts
@@ -0,0 +1,49 @@
+import type { TJSON } from "@skipruntime/client";
+import type {
+  Context,
+  Mapper,
+  EagerCollection,
+  NonEmptyIterator,
+  SkipService,
+  Resource,
+} from "@skipruntime/core";
+import { RemoteResources } from "@skipruntime/core";
+
+import { runService } from "@skipruntime/server";
+
+class Mult implements Mapper<string, number, string, number> {
+  mapElement(
+    key: string,
+    it: NonEmptyIterator<number>,
+  ): Iterable<[string, number]> {
+    const v = it.toArray();
+    return [[key, v.reduce((p, c) => p * c, 1)]];
+  }
+}
+
+class MultResource implements Resource {
+  reactiveCompute(
+    context: Context,
+    _collections: Record<string, EagerCollection<TJSON, TJSON>>,
+  ): EagerCollection<string, number> {
+    const sub = context.manageResource<string, number>("sumexample", "sub", {});
+    const add = context.manageResource<string, number>("sumexample", "add", {});
+    return sub.merge(add).map(Mult);
+  }
+}
+
+class Service implements SkipService {
+  resources = { data: MultResource };
+  remoteCollections = {
+    sumexample: RemoteResources.direct({ host: "localhost", port: 3587 }),
+  };
+
+  reactiveCompute(
+    _context: Context,
+    inputCollections: Record<string, EagerCollection<string, number>>,
+  ) {
+    return inputCollections;
+  }
+}
+
+await runService(new Service(), 3588);
