0e613d444659c1c58f8e630a608cc7c4fce1ba26
Lucas Hosseini <lucas@skiplabs.io>
2024-11-13 10:20:21 +0100

    [examples/hackernews] Move frontend to SSE.

--- examples/hackernews/front-end/src/main.tsx
+++ examples/hackernews/front-end/src/main.tsx
@@ -1,17 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { connect, Protocol } from "@skipruntime/client";
 import App from "./App.tsx";
-import { SkipContext } from "./SkipContext.ts";
 import "./index.css";
 
-const skipclient = await connect(
-  "ws://localhost:8080",
-  await Protocol.generateCredentials(),
-);
-
 const root = createRoot(document.getElementById("root")!);
-root.render(
-  <SkipContext.Provider value={skipclient}>
-    <App />
-  </SkipContext.Provider>,
-);
+root.render(<App />);
