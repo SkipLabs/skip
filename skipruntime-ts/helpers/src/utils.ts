30b026e45a1e5b64528655da98598bd60149d8d9
LOPES Daniel <daniel@skiplabs.io>
2024-11-19 09:27:54 +0100

    Manage resource instance identifier to manage URL REWRITE With SSE

--- skipruntime-ts/helpers/src/utils.ts
+++ skipruntime-ts/helpers/src/utils.ts
@@ -1,11 +1,6 @@
 import type { Nullable } from "@skip-wasm/std";
 import { ManyToOneMapper } from "@skipruntime/api";
-import type {
-  Reducer,
-  NonEmptyIterator,
-  ReactiveResponse,
-  Json,
-} from "@skipruntime/api";
+import type { Reducer, NonEmptyIterator, Json } from "@skipruntime/api";
 
 export class Sum implements Reducer<number, number> {
   default = 0;
@@ -50,21 +45,4 @@ export class CountMapper<
   mapValues(values: NonEmptyIterator<V>): number {
     return values.toArray().length;
   }
-}
-
-export function parseReactiveResponse(
-  header: Headers | string,
-): ReactiveResponse | undefined {
-  const strReactiveResponse =
-    typeof header == "string"
-      ? header
-      : header.get("Skip-Reactive-Response-Token");
-  if (!strReactiveResponse) return undefined;
-  return JSON.parse(strReactiveResponse) as ReactiveResponse;
-}
-
-export function reactiveResponseHeader(
-  reactiveResponse: ReactiveResponse,
-): [string, string] {
-  return ["Skip-Reactive-Response-Token", JSON.stringify(reactiveResponse)];
 }
