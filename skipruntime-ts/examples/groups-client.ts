393884c7d1b2292767b69e6ea55c3945a4cb8138
Benno Stein <benno.stein@gmail.com>
2024-11-21 13:18:08 +0000

    Pull getting-started example into skipruntime-ts/examples, add plumbing

---
+++ skipruntime-ts/examples/groups-client.ts
@@ -0,0 +1,34 @@
+// TODO: Remove once global `EventSource` makes it out of experimental
+// in nodejs LTS.
+import EventSource from "eventsource";
+import { fetchJSON } from "@skipruntime/helpers/rest.js";
+
+async function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+const restPort = 8990;
+const reactivePort = 8991;
+const restURL = `http://localhost:${restPort.toString()}`;
+
+console.log("Connecting to reactive server for resource /activeFriends");
+
+const evSource = new EventSource(
+  `http://localhost:${reactivePort.toString()}/v1/activeFriends`,
+);
+evSource.addEventListener("init", (e: MessageEvent<string>) => {
+  const updates = JSON.parse(e.data);
+  console.log("Initial data: ", updates);
+});
+evSource.addEventListener("update", (e: MessageEvent<string>) => {
+  const updates = JSON.parse(e.data);
+  console.log("Updated data: ", updates);
+});
+
+evSource.onerror = console.error;
+
+await sleep(1000);
+
+console.log(
+  "getting /activeFriends/1/1...",
+  await fetchJSON(`${restURL}/activeFriends/1/1`),
+);
---
+++ skipruntime-ts/examples/groups.ts
@@ -0,0 +1,109 @@
+import type {
+  EagerCollection,
+  InitialData,
+  Mapper,
+  NonEmptyIterator,
+  Resource,
+} from "@skipruntime/api";
+
+import { runService } from "@skipruntime/server";
+import { deepFreeze } from "skip-wasm";
+
+type UserID = number;
+type GroupID = number;
+type User = { name: string; active?: boolean; friends: UserID[] };
+type Group = { name: string; members: UserID[] };
+
+// Load initial data from a source-of-truth database (mocked for simplicity)
+const initialData: InitialData<ServiceInputs> = {
+  users: [
+    [1, [deepFreeze({ name: "Bob", active: true, friends: [2, 3] })]],
+    [2, [deepFreeze({ name: "Alice", active: true, friends: [1, 3] })]],
+    [3, [deepFreeze({ name: "Carol", active: false, friends: [1, 2] })]],
+    [4, [deepFreeze({ name: "Eve", active: true, friends: [] })]],
+  ],
+  groups: [
+    [1, [deepFreeze({ name: "Group1", members: [1, 2, 3, 4] })]],
+    [2, [deepFreeze({ name: "Group2", members: [1, 3, 4] })]],
+  ],
+};
+
+// Type alias for inputs to our service
+type ServiceInputs = {
+  users: EagerCollection<UserID, User>;
+  groups: EagerCollection<GroupID, Group>;
+};
+
+// Type alias for inputs to the active friends resource
+type ResourceInputs = {
+  users: EagerCollection<UserID, User>;
+  actives: EagerCollection<GroupID, UserID>;
+};
+
+// Mapper function to compute the active users of each group
+class ActiveUsers implements Mapper<GroupID, Group, GroupID, UserID> {
+  constructor(private users: EagerCollection<UserID, User>) {}
+  mapEntry(
+    gid: GroupID,
+    group: NonEmptyIterator<Group>,
+  ): Iterable<[GroupID, UserID]> {
+    const actives: Array<[GroupID, UserID]> = [];
+    for (const uid of group.getUnique().members) {
+      if (this.users.getUnique(uid).active) actives.push([gid, uid]);
+    }
+    return actives;
+    // Logic should be as follows, but WasmHandle proxies don't support filter/map???
+    //        return group
+    //          .getUnique()
+    //          .members.filter((uid) => this.users.getUnique(uid).active)
+    //          .map((uid) => [gid, uid]);
+  }
+}
+
+// Mapper function to filter out those active users who are also friends with `user`
+class FilterFriends implements Mapper<GroupID, UserID, GroupID, UserID> {
+  constructor(private user: User) {}
+  mapEntry(
+    gid: GroupID,
+    uids: NonEmptyIterator<UserID>,
+  ): Iterable<[GroupID, UserID]> {
+    return uids
+      .toArray()
+      .reduce(
+        (acc, uid) =>
+          this.user.friends.includes(uid) ? [...acc, [gid, uid]] : acc,
+        [] as Array<[GroupID, UserID]>,
+      );
+  }
+}
+
+class ActiveFriends implements Resource<ResourceInputs> {
+  private uid: UserID;
+  constructor(params: { [param: string]: string }) {
+    if (!params["uid"]) throw new Error("Missing required parameter 'uid'");
+    this.uid = parseInt(params["uid"]);
+  }
+  instantiate(inputs: ResourceInputs): EagerCollection<GroupID, UserID> {
+    const user = inputs.users.getUnique(this.uid);
+    return inputs.actives.map(FilterFriends, user);
+  }
+}
+
+// Specify and run the reactive service
+const service = await runService(
+  {
+    initialData,
+    resources: { activeFriends: ActiveFriends },
+    createGraph(input: ServiceInputs): ResourceInputs {
+      const actives = input.groups.map(ActiveUsers, input.users);
+      return { users: input.users, actives };
+    },
+  },
+  8991,
+);
+function shutdown() {
+  service.close();
+}
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
