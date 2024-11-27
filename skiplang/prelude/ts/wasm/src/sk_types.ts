516f40f8458ce83eac0f4eecfeaaac31471707da
Mehdi Bouaziz <mehdi@skiplabs.io>
2024-11-27 11:23:23 +0100

    Allow Environment.fetch to return ArrayBuffer
    WebAssembly.instantiate like it as much as Uint8Array
    Saves a conversion

--- skiplang/prelude/ts/wasm/src/sk_types.ts
+++ skiplang/prelude/ts/wasm/src/sk_types.ts
@@ -166,7 +166,7 @@ export interface Environment {
   fs: () => FileSystem;
   sys: () => System;
   crypto: () => Crypto;
-  fetch: (url: URL | string) => Promise<Uint8Array>;
+  fetch: (url: URL | string) => Promise<Uint8Array | ArrayBuffer>;
 }
 
 export interface Memory {
@@ -839,7 +839,7 @@ export function loadWasm(
 
 async function start(
   modules: ModuleInit[],
-  buffer: Uint8Array,
+  buffer: Uint8Array | ArrayBuffer,
   environment: Environment,
   main?: string,
 ) {
@@ -901,7 +901,7 @@ export async function run(
   getWasmSource?: () => Promise<Uint8Array>,
 ) {
   const env = await loadEnv(extensions);
-  let buffer: Uint8Array;
+  let buffer: Uint8Array | ArrayBuffer;
   if (getWasmSource) {
     buffer = await getWasmSource();
   } else {
