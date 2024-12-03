e7251d1c94859f6b5d00a922d6d681e474c9e64d
Benno Stein <benno.stein@gmail.com>
2024-12-03 17:27:19 +0000

    Switch from Mapper to OneToOneMapper in groups example

--- skipruntime-ts/examples/groups.ts
+++ skipruntime-ts/examples/groups.ts
@@ -1,9 +1,8 @@
-import type {
-  EagerCollection,
-  InitialData,
-  Mapper,
-  NonEmptyIterator,
-  Resource,
+import {
+  type EagerCollection,
+  type InitialData,
+  type Resource,
+  OneToOneMapper,
 } from "@skipruntime/api";
 
 import { runService } from "@skipruntime/server";
@@ -36,37 +35,28 @@ type ServiceInputs = {
 // Type alias for inputs to the active friends resource
 type ResourceInputs = {
   users: EagerCollection<UserID, User>;
-  actives: EagerCollection<GroupID, UserID>;
+  actives: EagerCollection<GroupID, UserID[]>;
 };
 
 // Mapper function to compute the active users of each group
-class ActiveUsers implements Mapper<GroupID, Group, GroupID, UserID> {
-  constructor(private users: EagerCollection<UserID, User>) {}
+class ActiveUsers extends OneToOneMapper<GroupID, Group, UserID[]> {
+  constructor(private users: EagerCollection<UserID, User>) {
+    super();
+  }
 
-  mapEntry(
-    gid: GroupID,
-    group: NonEmptyIterator<Group>,
-  ): Iterable<[GroupID, UserID]> {
-    return group
-      .getUnique()
-      .members.filter((uid) => this.users.getUnique(uid).active)
-      .map((uid) => [gid, uid]);
+  mapValue(group: Group): UserID[] {
+    return group.members.filter((uid) => this.users.getUnique(uid).active);
   }
 }
 
 // Mapper function to filter out those active users who are also friends with `user`
-class FilterFriends implements Mapper<GroupID, UserID, GroupID, UserID> {
-  constructor(private readonly user: User) {}
+class FilterFriends extends OneToOneMapper<GroupID, UserID[], UserID[]> {
+  constructor(private readonly user: User) {
+    super();
+  }
 
-  mapEntry(
-    gid: GroupID,
-    uids: NonEmptyIterator<UserID>,
-  ): Iterable<[GroupID, UserID]> {
-    return uids
-      .toArray()
-      .reduce<
-        [GroupID, UserID][]
-      >((acc, uid) => (this.user.friends.includes(uid) ? [...acc, [gid, uid]] : acc), []);
+  mapValue(uids: UserID[]): UserID[] {
+    return uids.filter((uid) => this.user.friends.includes(uid));
   }
 }
 
@@ -78,7 +68,7 @@ class ActiveFriends implements Resource<ResourceInputs> {
     this.uid = params["uid"];
   }
 
-  instantiate(inputs: ResourceInputs): EagerCollection<GroupID, UserID> {
+  instantiate(inputs: ResourceInputs): EagerCollection<GroupID, UserID[]> {
     const user = inputs.users.getUnique(this.uid);
     return inputs.actives.map(FilterFriends, user);
   }
