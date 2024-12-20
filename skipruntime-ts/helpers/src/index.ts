6eee8dd8f9a2d8a3e2a7204045f0afcc6f66df1a
Benno Stein <benno.stein@gmail.com>
2024-12-20 19:47:32 +0000

    Add native reducers; plumb through FFI using stubs on TS side

--- skipruntime-ts/helpers/src/index.ts
+++ skipruntime-ts/helpers/src/index.ts
@@ -4,11 +4,4 @@ export {
   Polled,
 } from "./external.js";
 export { SkipServiceBroker, fetchJSON, type Entrypoint } from "./rest.js";
-export {
-  Count,
-  CountMapper,
-  Max,
-  Min,
-  SkipExternalService,
-  Sum,
-} from "@skipruntime/core";
+export { Count, Max, Min, SkipExternalService, Sum } from "@skipruntime/core";
