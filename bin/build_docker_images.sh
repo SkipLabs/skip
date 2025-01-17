1e2ba8d5a0b6d600b674526818940e089e9eec87
Mehdi Bouaziz <mehdi@skiplabs.io>
2025-01-17 15:34:10 +0100

    Merge build_prod_skdb_images.sh into build_docker_images.sh
    Reduce copy-paste

--- bin/build_docker_images.sh
+++ bin/build_docker_images.sh
@@ -6,19 +6,34 @@ REPO="$SCRIPT_DIR/.."
 set -e
 set -x
 
+if [ "$1" = "--prod" ]; then
+  DOCKERBUILD_EXTRA=("--platform=linux/amd64")
+  PROD=true
+else
+  DOCKERBUILD_EXTRA=()
+  PROD=false
+fi
+
 cd "$REPO"
 
 git clean -xd --dry-run | sed 's|Would remove |/|g' >> .dockerignore
 echo ".git" >> .dockerignore
 
 dockerbuild () {
-    docker build . --no-cache --progress=plain --tag "$1" --file "$2/Dockerfile"
+    local tag="$1"
+    local file="$2/Dockerfile"
+    shift 2
+    docker build . --no-cache --progress=plain --tag "$tag" --file "$file" "$@"
 }
 
-dockerbuild skiplabs/skdb-base .
-dockerbuild skiplabs/skdb sql
-dockerbuild skiplabs/skdb-dev-server sql/server/dev
-[[ -f $USER/Dockerfile ]] && dockerbuild "$USER-skdb" "$USER"
+dockerbuild skiplabs/skdb-base . "${DOCKERBUILD_EXTRA[@]}"
+dockerbuild skiplabs/skdb sql "${DOCKERBUILD_EXTRA[@]}"
+if $PROD; then
+  dockerbuild skiplabs/server-core sql/server/core
+else
+  dockerbuild skiplabs/skdb-dev-server sql/server/dev
+  [[ -f $USER/Dockerfile ]] && dockerbuild "$USER-skdb" "$USER"
+fi
 
 git restore .dockerignore
 
--- bin/build_prod_skdb_images.sh
+++ bin/build_prod_skdb_images.sh
@@ -1,28 +1,6 @@
 #! /bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-REPO="$SCRIPT_DIR/.."
-
 set -e
 set -x
 
-cd "$REPO"
-
-git clean -xd --dry-run | sed 's|Would remove |/|g' >> .dockerignore
-echo ".git" >> .dockerignore
-
-dockerbuild () {
-    docker build . --no-cache --progress=plain --tag "$1" --file "$2/Dockerfile" "$3"
-}
-
-dockerbuild skiplabs/skdb-base . --platform=linux/amd64
-dockerbuild skiplabs/skdb sql --platform=linux/amd64
-dockerbuild skiplabs/server-core sql/server/core
-
-git restore .dockerignore
-
-echo disk usage:
-docker system df
-
-echo images:
-docker images
+"$(dirname -- "${BASH_SOURCE[0]}")"/build_docker_images.sh --prod
