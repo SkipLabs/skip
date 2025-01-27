01741754928ab8520072e7e93de8be509fb8be12
Josh Berdine <josh@skiplabs.io>
2025-01-27 13:25:47 +0000

    Replace old url

--- www/docusaurus.config.ts
+++ www/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.svg",
 
   // Set the production url of your site here
-  url: "https://skiplabsdocscms-00-pbrstreetgang-00s-projects.vercel.app/",
+  url: "https://skiplabs.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
