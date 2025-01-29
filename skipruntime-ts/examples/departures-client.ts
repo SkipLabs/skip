579b623bc3c37240c6cff0511a4c55461bb46ff7
Benno Stein <benno.stein@gmail.com>
2025-01-29 13:40:19 +0000

    Tweak sleep timings for running examples in CI

--- skipruntime-ts/examples/departures-client.ts
+++ skipruntime-ts/examples/departures-client.ts
@@ -9,27 +9,32 @@ const service = new SkipServiceBroker({
   streaming_port,
 });
 
+// Time to wait (in milliseconds) for local/remote operations to complete,
+// allowing for reproducible/consistent test output
+const local_delay = 10;
+const remote_delay = 2000;
+
 console.log(JSON.stringify(await service.getAll("departures", {})));
-await sleep(10);
+await sleep(local_delay);
 const closable = await subscribe(service, "departures", streaming_port);
-await sleep(10);
+await sleep(local_delay);
 await service.update("config", [["year", [[2015]]]]);
-await sleep(1000);
+await sleep(remote_delay);
 console.log(JSON.stringify(await service.getAll("departures", {})));
-await sleep(10);
+await sleep(local_delay);
 await service.update("config", [["origin", [["SYR"]]]]);
-await sleep(1000);
+await sleep(remote_delay);
 console.log(JSON.stringify(await service.getAll("departures", {})));
-await sleep(10);
+await sleep(local_delay);
 await service.update("config", [["resettlement", [["USA"]]]]);
-await sleep(1000);
+await sleep(remote_delay);
 console.log(JSON.stringify(await service.getAll("departures", {})));
-await sleep(10);
+await sleep(local_delay);
 await service.update("config", [
   ["year", [[2016, 2017]]],
   ["origin", [["MMR", "SYR"]]],
   ["resettlement", [["NOR", "USA"]]],
 ]);
-await sleep(1000);
+await sleep(remote_delay);
 console.log(JSON.stringify(await service.getAll("departures", {})));
 closable.close();
