09b8a7c0eb2a96ac7093a2f120bd2818a0e39eda
Benno Stein <benno.stein@gmail.com>
2025-01-29 12:21:34 +0000

    Simplify examples setup and apply minor fixes throughout

--- skipruntime-ts/examples/remote-client.ts
+++ skipruntime-ts/examples/remote-client.ts
@@ -1,45 +1,38 @@
-import { SkipHttpAccessV1, run, type Step } from "./utils.js";
+import { sleep, subscribe } from "./utils.js";
+import { SkipServiceBroker } from "@skipruntime/helpers";
 
-function scenarios() {
-  return [
-    [
-      {
-        type: "write",
-        payload: [{ collection: "input1", entries: [["v1", [2]]] }],
-      },
-      {
-        type: "write",
-        payload: [{ collection: "input2", entries: [["v1", [3]]] }],
-      },
-      {
-        type: "delete",
-        payload: [{ collection: "input1", keys: ["v1"] }],
-      },
-      {
-        type: "write",
-        payload: [
-          {
-            collection: "input1",
-            entries: [
-              ["v1", [2]],
-              ["v2", [6]],
-            ],
-          },
-        ],
-      },
-      {
-        type: "write",
-        payload: [{ collection: "input2", entries: [["v2", [0]]] }],
-      },
-      {
-        type: "write",
-        payload: [{ collection: "input1", entries: [["v1", [8]]] }],
-      },
-    ] as Step[],
-  ];
-}
+const sum_streaming_port = 3587;
+const sum_control_port = 3588;
+const sum_service = new SkipServiceBroker({
+  host: "localhost",
+  control_port: sum_control_port,
+  streaming_port: sum_streaming_port,
+});
 
-const access = new SkipHttpAccessV1(3589, 3590);
-access.request("data", {});
+const remote_streaming_port = 3589;
+const remote_control_port = 3590;
+const remote_service = new SkipServiceBroker({
+  host: "localhost",
+  control_port: remote_control_port,
+  streaming_port: remote_streaming_port,
+});
 
-run(scenarios(), 3587, 3588);
+const closable = await subscribe(remote_service, "data", remote_streaming_port);
+
+await sleep(10);
+await sum_service.update("input1", [["v1", [2]]]);
+await sleep(10);
+await sum_service.update("input2", [["v1", [3]]]);
+await sleep(10);
+await sum_service.deleteKey("input1", "v1");
+await sleep(10);
+await sum_service.update("input1", [
+  ["v1", [2]],
+  ["v2", [6]],
+]);
+await sleep(10);
+await sum_service.update("input2", [["v2", [0]]]);
+await sleep(10);
+await sum_service.update("input1", [["v1", [8]]]);
+await sleep(10);
+closable.close();
