41c3537468119727db41ffc580c0f0d0ea638da2
Mehdi Bouaziz <mehdi@skiplabs.io>
2025-01-29 10:16:16 +0100

    Split script as done in Dockerfile

--- bin/apt-install.sh
+++ bin/apt-install.sh
@@ -8,14 +8,10 @@ PRIORITY=100
 apt-get update
 apt-get install -q -y wget gnupg
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
-wget -O - https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | apt-key add -
 echo "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-$LLVM_VERSION main" >> /etc/apt/sources.list.d/llvm.list
 echo "deb-src http://apt.llvm.org/jammy/ llvm-toolchain-jammy-$LLVM_VERSION main" >> /etc/apt/sources.list.d/llvm.list
-echo "deb https://deb.nodesource.com/node_22.x nodistro main" >> /etc/apt/sources.list.d/nodejs.list
 apt-get update
-apt-get install -q -y automake clang-$LLVM_VERSION clang-format-$LLVM_VERSION curl file gawk gcc git jq lld-$LLVM_VERSION llvm-$LLVM_VERSION make nodejs parallel unzip zip
-npm install -g bun
-npm install -g prettier
+apt-get install -q -y automake clang-$LLVM_VERSION clang-format-$LLVM_VERSION curl file gawk gcc git jq lld-$LLVM_VERSION llvm-$LLVM_VERSION make parallel unzip zip
 
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$LLVM_VERSION $PRIORITY
 update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-$LLVM_VERSION $PRIORITY
@@ -25,3 +21,10 @@ update-alternatives --install /usr/bin/llvm-ar llvm-ar /usr/bin/llvm-ar-$LLVM_VE
 update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-$LLVM_VERSION $PRIORITY
 update-alternatives --install /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-$LLVM_VERSION $PRIORITY
 update-alternatives --install /usr/bin/wasm-ld wasm-ld /usr/bin/wasm-ld-$LLVM_VERSION $PRIORITY
+
+wget -O - https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | apt-key add -
+echo "deb https://deb.nodesource.com/node_22.x nodistro main" >> /etc/apt/sources.list.d/nodejs.list
+apt-get update
+apt-get install -q -y nodejs
+npm install -g bun
+npm install -g prettier
