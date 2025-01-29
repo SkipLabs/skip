320662f444c0a2ef1d1787b6230d78adab0de5fd
Benno Stein <benno.stein@gmail.com>
2025-01-29 13:40:19 +0000

    Tweak sleep timings for running examples in CI

--- skipruntime-ts/examples/groups-client.ts
+++ skipruntime-ts/examples/groups-client.ts
@@ -28,30 +28,30 @@ evSource.onerror = console.error;
 /*       BEGIN SCENARIO OF CHANGING INPUTS        */
 /**************************************************/
 
-await sleep(1000);
+await sleep(100);
 console.log("Setting Carol to active...");
 await fetchJSON(`${url}/users/2`, "PUT", {
   body: { name: "Carol", active: true, friends: [0, 1] },
 });
 
-await sleep(1000);
+await sleep(100);
 console.log("Setting Alice to inactive...");
 await fetchJSON(`${url}/users/1`, "PUT", {
   body: { name: "Alice", active: false, friends: [0, 2] },
 });
 
-await sleep(1000);
+await sleep(100);
 console.log("Setting Eve as Bob's friend...");
 await fetchJSON(`${url}/users/0`, "PUT", {
   body: { name: "Bob", active: true, friends: [1, 2, 3] },
 });
 
-await sleep(1000);
+await sleep(100);
 console.log("Removing Carol and adding Eve to group 2...");
 await fetchJSON(`${url}/groups/1002`, "PUT", {
   body: { name: "Group 2", members: [0, 3] },
 });
 
-await sleep(1000);
+await sleep(100);
 console.log("Closing listener event stream...");
 evSource.close();
