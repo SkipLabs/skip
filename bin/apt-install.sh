2e4352406aa5294b08423f634f6ae8146941e554
Mehdi Bouaziz <mehdi@skiplabs.io>
2025-01-31 10:35:16 +0100

    Install alternatives as slaves of clang
    To ensure all are changed when clang is changed

--- bin/apt-install.sh
+++ bin/apt-install.sh
@@ -4,7 +4,7 @@ set -e
 
 default_steps=(skiplang-build-deps skipruntime-deps)
 LLVM_VERSION=15
-PRIORITY=100
+PRIORITY=101
 
 usage() {
     echo "Usage: $0 step*"
@@ -32,14 +32,14 @@ for step in "${steps[@]}"; do
             apt-get update
             apt-get install -q -y automake clang-$LLVM_VERSION clang-format-$LLVM_VERSION curl file gawk gcc git jq lld-$LLVM_VERSION llvm-$LLVM_VERSION make parallel unzip zip
 
-            update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$LLVM_VERSION $PRIORITY
-            update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-$LLVM_VERSION $PRIORITY
-            update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-$LLVM_VERSION $PRIORITY
-            update-alternatives --install /usr/bin/llc llc /usr/bin/llc-$LLVM_VERSION $PRIORITY
-            update-alternatives --install /usr/bin/llvm-ar llvm-ar /usr/bin/llvm-ar-$LLVM_VERSION $PRIORITY
-            update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-$LLVM_VERSION $PRIORITY
-            update-alternatives --install /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-$LLVM_VERSION $PRIORITY
-            update-alternatives --install /usr/bin/wasm-ld wasm-ld /usr/bin/wasm-ld-$LLVM_VERSION $PRIORITY
+            update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$LLVM_VERSION $PRIORITY \
+                --slave /usr/bin/clang++ clang++ /usr/bin/clang++-$LLVM_VERSION \
+                --slave /usr/bin/clang-format clang-format /usr/bin/clang-format-$LLVM_VERSION \
+                --slave /usr/bin/llc llc /usr/bin/llc-$LLVM_VERSION \
+                --slave /usr/bin/llvm-ar llvm-ar /usr/bin/llvm-ar-$LLVM_VERSION \
+                --slave /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-$LLVM_VERSION \
+                --slave /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-$LLVM_VERSION \
+                --slave /usr/bin/wasm-ld wasm-ld /usr/bin/wasm-ld-$LLVM_VERSION
             ;;
         skipruntime-deps)
             wget -O - https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | apt-key add -
