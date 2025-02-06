57fe3da51bacb5cdcbc9c9e724f871d34649a0cf
Lucas Hosseini <lucas@skiplabs.io>
2025-02-06 11:24:09 +0100

    [examples/hackernews] Fix `runService()` call following #668.

--- examples/hackernews/reactive_service/server.js
+++ examples/hackernews/reactive_service/server.js
@@ -1,5 +1,4 @@
 import { runService } from "@skipruntime/server";
-import { initService } from "@skipruntime/wasm";
 import { service } from "./dist/hackernews.service.js";
 
-runService(await initService(service));
+runService(service);
