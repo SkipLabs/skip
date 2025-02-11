970d1f4fd84c96b5e8a8ebc885354c49d362b72f
LOPES Daniel <daniel@skiplabs.io>
2025-02-11 16:46:48 +0100

    Fix order in some example

--- skipruntime-ts/examples/sum.ts
+++ skipruntime-ts/examples/sum.ts
@@ -10,18 +10,32 @@ import { runService } from "@skipruntime/server";
 const platform: "wasm" | "native" =
   process.env["SKIP_PLATFORM"] == "native" ? "native" : "wasm";
 
+class AddIndex implements Mapper<string, number, string, [number, number]> {
+  constructor(private index: number) {}
+
+  mapEntry(
+    key: string,
+    values: Values<number>,
+  ): Iterable<[string, [number, number]]> {
+    return [[key, [this.index, values.getUnique()]]];
+  }
+}
+
 class Plus implements Mapper<string, number, string, number> {
   mapEntry(key: string, values: Values<number>): Iterable<[string, number]> {
     return [[key, values.toArray().reduce((p, c) => p + c, 0)]];
   }
 }
 
-class Minus implements Mapper<string, number, string, number> {
-  mapEntry(key: string, values: Values<number>): Iterable<[string, number]> {
-    const acc = (p: number | null, c: number) => {
-      return p !== null ? p - c : c;
+class Minus implements Mapper<string, [number, number], string, number> {
+  mapEntry(
+    key: string,
+    values: Values<[number, number]>,
+  ): Iterable<[string, number]> {
+    const acc = (p: number | null, c: [number, number]) => {
+      return p !== null ? p - c[1] : c[1];
     };
-    return [[key, values.toArray().reduce(acc, null) ?? 0]];
+    return [[key, values.toArray().sort().reduce(acc, null) ?? 0]];
   }
 }
 
@@ -38,7 +52,10 @@ class Add implements Resource<Collections> {
 
 class Sub implements Resource<Collections> {
   instantiate(cs: Collections): EagerCollection<string, number> {
-    return cs.input1.merge(cs.input2).map(Minus);
+    return cs.input1
+      .map(AddIndex, 0)
+      .merge(cs.input2.map(AddIndex, 1))
+      .map(Minus);
   }
 }
 
