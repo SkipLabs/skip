4b81a104879cd8f686ff381bc43f43ac42f74e49
LOPES Daniel <daniel@skiplabs.io>
2025-02-11 15:18:04 +0100

    Run the test example also with native platform

--- skipruntime-ts/tests/examples/remote.sh
+++ skipruntime-ts/tests/examples/remote.sh
@@ -2,14 +2,25 @@
 set -e
 cd ../../examples
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <stdout target> <stderr target>"
+cleanup() {
+    jobs -p | xargs kill
+}
+
+trap cleanup EXIT
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <stdout target> <stderr target> <platform>"
 fi
 
-node dist/sum.js >/dev/null &
-node dist/remote.js >/dev/null &
+if [ "$3" = "native" ]; then
+    echo "Run remote with native platform"
+    LD_LIBRARY_PATH=$(realpath ../../build/skipruntime) SKIP_PLATFORM="native" node dist/sum.js >/dev/null &
+    LD_LIBRARY_PATH=$(realpath ../../build/skipruntime) SKIP_PLATFORM="native" node dist/remote.js >/dev/null &
+else
+    echo "Run remote with wasm platform"
+    node dist/sum.js >/dev/null &
+    node dist/remote.js >/dev/null &
+fi
 
 sleep 1 # give a moment for services to spin up
 node dist/remote-client.js >"$1" 2>"$2"
-
-jobs -p | xargs kill
