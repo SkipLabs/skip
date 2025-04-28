ffd1f335c82f7e1a8db541baa57b4a787e04d09b
Benno Stein <benno.stein@gmail.com>
2025-04-28 14:55:30 +0100

    Add Kubernetes configuration for hackernews example

--- examples/hackernews/reactive_service/server.js
+++ examples/hackernews/reactive_service/server.js
@@ -4,20 +4,20 @@ import { service } from "./dist/hackernews.service.js";
 
 if (process.env["SKIP_LEADER"] == "true") {
   console.log("Running as leader...");
-  runService(asLeader(service));
+  await runService(asLeader(service)).catch(console.error);
 } else if (process.env["SKIP_FOLLOWER"] == "true") {
   console.log("Running as follower...");
-  runService(
+  await runService(
     asFollower(service, {
       leader: {
-        host: "skip_leader",
+        host: process.env["SKIP_LEADER_HOST"] || "skip_leader",
         streaming_port: 8080,
         control_port: 8081,
       },
       collections: ["postsWithUpvotes", "sessions"],
     }),
-  );
+  ).catch(console.error);
 } else {
   console.log("Running non-distributed...");
-  runService(service);
+  await runService(service).catch(console.error);
 }
