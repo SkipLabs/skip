7a100698e4eec20fc0229366722da2b810a0bcca
LOPES Daniel <daniel@skiplabs.io>
2025-08-06 18:02:42 +0200

    Add resource reload tests

--- skipruntime-ts/tests/src/for-reload.ts
+++ skipruntime-ts/tests/src/for-reload.ts
@@ -1,7 +1,26 @@
-import type { Mapper, Values } from "@skipruntime/core";
+import type {
+  Mapper,
+  Values,
+  Resource,
+  EagerCollection,
+} from "@skipruntime/core";
 
 export class ReloadMapper implements Mapper<number, number, number, number> {
   mapEntry(key: number, values: Values<number>): Iterable<[number, number]> {
     return Array([0, values.getUnique() + 2 * key]);
+  }
+}
+
+export class AfterMapper implements Mapper<number, number, number, number> {
+  mapEntry(key: number, values: Values<number>): Iterable<[number, number]> {
+    return values.toArray().map((v) => [key + 1, v + 1] as [number, number]);
+  }
+}
+
+type Input_NN = { input: EagerCollection<number, number> };
+
+export class ReloadResource implements Resource<Input_NN> {
+  instantiate(collections: Input_NN): EagerCollection<number, number> {
+    return collections.input.map(ReloadMapper).map(AfterMapper);
   }
 }
